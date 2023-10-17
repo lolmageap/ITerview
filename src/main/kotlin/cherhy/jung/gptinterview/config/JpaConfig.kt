3e8e82ced81e9610006b8542b9dd08216ed51c7d
lolmageap <ekxk1234@gmail.com>
2023-10-17 18:03:23 +0900

    JPA 설정

---
+++ src/main/kotlin/cherhy/jung/gptinterview/config/JpaConfig.kt
@@ -0,0 +1,19 @@
+package cherhy.jung.gptinterview.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer
+
+@Configuration
+@EnableJpaAuditing
+class JpaConfig {
+
+    @Bean
+    fun customize(): PageableHandlerMethodArgumentResolverCustomizer? {
+        return PageableHandlerMethodArgumentResolverCustomizer {
+            it.setOneIndexedParameters(true)
+        }
+    }
+
+}
