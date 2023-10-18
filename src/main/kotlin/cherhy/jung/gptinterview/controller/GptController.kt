8aa7973f88fbd4d33c5d206d6e0e4e3c845b89a1
cherhy <ekxk1234@naver.com>
2023-10-18 23:53:58 +0900

    rename

--- src/main/kotlin/cherhy/jung/gptinterview/controller/GptController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/GptController.kt
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HomeController(private val gptApi: GptApi) {
+class GptController(private val gptApi: GptApi) {
 
     @PostMapping
-    suspend fun get(@RequestBody question: String) = gptApi.generateText(question)
+    fun get(@RequestBody question: String) = gptApi.generateText(question)
 
 }
