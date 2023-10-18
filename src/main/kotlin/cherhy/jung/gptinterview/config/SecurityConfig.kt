f544173d959ed1a8d92ae2c3a2b47f73605eca65
cherhy <ekxk1234@naver.com>
2023-10-18 23:54:43 +0900

    create spring security configuration

---
+++ src/main/kotlin/cherhy/jung/gptinterview/config/SecurityConfig.kt
@@ -0,0 +1,26 @@
+package cherhy.jung.gptinterview.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+
+    @Bean
+    @Throws(Exception::class)
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain? {
+        http.authorizeHttpRequests { requests ->
+            requests
+                .requestMatchers("/**").permitAll()
+                .anyRequest().authenticated()
+        }.csrf { it.disable() }
+
+        return http.build()
+    }
+
+}
