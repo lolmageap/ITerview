a434aaf568043c43695dbd510f2a02eab1e1974c
lolmageap <ekxk1234@naver.com>
2023-10-22 22:07:29 +0900

    create redis configuration

---
+++ src/main/kotlin/cherhy/jung/gptinterview/config/RedisConfig.kt
@@ -0,0 +1,22 @@
+package cherhy.jung.gptinterview.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
+
+@Configuration
+class RedisConfig {
+
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory()
+    }
+
+    @Bean
+    fun redisTemplate(redisConnectionFactory: LettuceConnectionFactory): RedisTemplate<String, Any> {
+        return RedisTemplate<String, Any>().also {
+            it.connectionFactory = redisConnectionFactory
+        }
+    }
+}
