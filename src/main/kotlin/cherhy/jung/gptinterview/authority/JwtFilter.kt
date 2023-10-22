57bf449cecfaaee9f7db34d2f498cec1aa701c78
lolmageap <ekxk1234@naver.com>
2023-10-22 20:12:07 +0900

    edit resolved token and doFilter method

--- src/main/kotlin/cherhy/jung/gptinterview/authority/JwtFilter.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/authority/JwtFilter.kt
@@ -1,11 +1,12 @@
 package cherhy.jung.gptinterview.authority
 
-import cherhy.jung.gptinterview.util.log
+import cherhy.jung.gptinterview.component.RedisReadService
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletException
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.security.access.AccessDeniedException
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.util.StringUtils
@@ -13,7 +14,10 @@ import org.springframework.web.filter.GenericFilterBean
 import java.io.IOException
 
 
-class JwtFilter(private val tokenProvider: TokenProvider) : GenericFilterBean() {
+class JwtFilter(
+    private val tokenProvider: TokenProvider,
+    private val redisReadService: RedisReadService,
+) : GenericFilterBean() {
 
     companion object {
         const val AUTHORIZATION_HEADER = "Authorization"
@@ -22,23 +26,26 @@ class JwtFilter(private val tokenProvider: TokenProvider) : GenericFilterBean() 
     @Throws(IOException::class, ServletException::class)
     override fun doFilter(servletRequest: ServletRequest, servletResponse: ServletResponse?, filterChain: FilterChain) {
         val httpServletRequest: HttpServletRequest = servletRequest as HttpServletRequest
-        val jwt: String? = resolveToken(httpServletRequest)
-        val requestURI: String = httpServletRequest.requestURI
-        if (StringUtils.hasText(jwt) && tokenProvider.validateToken(jwt)) {
-            val authentication: Authentication? = tokenProvider.getAuthentication(jwt)
-            SecurityContextHolder.getContext().authentication = authentication
+        val jwt: String = getResolveToken(httpServletRequest)
+
+        val authentication: Authentication? = if (tokenProvider.validateToken(jwt)) {
+            tokenProvider.getAuthentication(jwt)
         } else {
-            log.debug("유효한 JWT 토큰이 없습니다, uri: {}", requestURI)
+            val refreshToken = redisReadService.getJwtToken(jwt) ?: throw AccessDeniedException("잘못된 토큰")
+            tokenProvider.getAuthentication(refreshToken)
         }
+
+        SecurityContextHolder.getContext().authentication = authentication
         filterChain.doFilter(servletRequest, servletResponse)
     }
 
-    private fun resolveToken(request: HttpServletRequest): String? {
+
+    private fun getResolveToken(request: HttpServletRequest): String {
         val bearerToken: String = request.getHeader(AUTHORIZATION_HEADER)
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             return bearerToken.substring(7)
         }
-        return null
+        throw AccessDeniedException("잘못된 토큰")
     }
 
 }
