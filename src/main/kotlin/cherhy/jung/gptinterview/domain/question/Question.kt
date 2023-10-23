b4cf3bd2760d3e55553212bf37ea5076af748781
cherhy <ekxk1234@naver.com>
2023-10-23 22:41:45 +0900

    add field to Question Entity

---
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/Question.kt
@@ -0,0 +1,18 @@
+package cherhy.jung.gptinterview.domain.question
+
+import cherhy.jung.gptinterview.util.BaseEntity
+import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+
+@Entity
+class Question(
+    val title: String,
+
+    @Enumerated(EnumType.STRING)
+    val questionType: QuestionType,
+
+    val token: String,
+) : BaseEntity() {
+
+}
