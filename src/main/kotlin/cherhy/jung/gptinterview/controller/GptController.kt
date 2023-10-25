cbe0c8719124aeb2bd4644a2f77819431614fbc3
cherhy <ekxk1234@naver.com>
2023-10-25 23:02:57 +0900

    rename post method

--- src/main/kotlin/cherhy/jung/gptinterview/controller/GptController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/GptController.kt
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController
 class GptController(private val gptApi: GptApi) {
 
     @PostMapping
-    fun get(@RequestBody question: String) = gptApi.generateText(question)
+    fun postAndGet(@RequestBody question: String) = gptApi.generateText(question)
 
 }
