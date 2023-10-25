7f7d76c2628632f4d0bfbd7201f6d37ed0810a22
cherhy <ekxk1234@naver.com>
2023-10-25 21:11:00 +0900

    delete description

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/FrameworkType.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/FrameworkType.kt
@@ -1,17 +1,16 @@
 package cherhy.jung.gptinterview.domain.question
 
-enum class FrameworkType(private val language: ProgramingType) {
-    SPRING(ProgramingType.JAVA),
-    VIEW(ProgramingType.JAVASCRIPT),
-    REACT(ProgramingType.JAVASCRIPT),
-    JQUERY(ProgramingType.JAVASCRIPT),
-    ANGULAR(ProgramingType.JAVASCRIPT),
-    EXPRESS(ProgramingType.JAVASCRIPT),
-    NEST(ProgramingType.JAVASCRIPT),
-    DJANGO(ProgramingType.PYTHON),
-    FLASK(ProgramingType.PYTHON),
-    FAST_API(ProgramingType.PYTHON),
-    KTOR(ProgramingType.KOTLIN),
-    DOTNET(ProgramingType.C),
-    LARAVEL(ProgramingType.PHP)
+enum class FrameworkType() {
+    SPRING,
+    VIEW,
+    REACT,
+    ANGULAR,
+    EXPRESS,
+    NEST,
+    DJANGO,
+    FLASK,
+    FAST_API,
+    UNITY,
+    DOTNET,
+    LARAVEL,
 }
--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionType.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionType.kt
@@ -1,15 +1,14 @@
 package cherhy.jung.gptinterview.domain.question
 
-enum class QuestionType(private val text: String) {
-    DATABASE("데이터베이스"),
-    NETWORK("네트워크"),
-    DESIGN_PATTERN("디자인 패턴"),
-    TEST("테스트"),
-    OS("운영체제"),
-    STRUCTURE("자료구조"),
-    ALGORITHM("알고리즘"),
-    PROGRAMING("프로그래밍"),
-    FRAMEWORK("프레임워크"),
-    VCS("형상관리"),
-    HARDWARE("컴퓨터 구조"),
+enum class QuestionType() {
+    DATABASE,
+    NETWORK,
+    DESIGN_PATTERN,
+    TEST,
+    OS,
+    STRUCTURE,
+    ALGORITHM,
+    PROGRAMING,
+    FRAMEWORK,
+    VCS,
 }
