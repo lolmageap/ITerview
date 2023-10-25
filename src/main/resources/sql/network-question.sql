fc3d39168f35483dc2cc5375152e6d0dad572577
cherhy <ekxk1234@naver.com>
2023-10-25 23:01:10 +0900

    create sql file

---
+++ src/main/resources/sql/network-question.sql
@@ -0,0 +1,117 @@
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'OSI 7계층에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'TCP/IP 모델과 OSI 모델의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'HTTP와 HTTPS의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '서버와 클라이언트 간의 통신 과정을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'IP 주소와 포트 번호의 역할에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '라우팅이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '서브넷이 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '서브넷팅(Subnetting)이 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'NAT(Network Address Translation)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'DNS의 역할과 작동 원리를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'VPN(Virtual Private Network)이 무엇이며 어떻게 작동하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '라우터와 스위치의 차이점을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '포트 포워딩(Port Forwarding)이 무엇이며 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'OSI 7계층 중 전송 계층(Transport Layer)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'OSI 7계층 중 네트워크 계층(Network Layer)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'OSI 7계층 중 데이터 링크 계층(Data Link Layer)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'OSI 7계층 중 물리 계층(Physical Layer)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'ICMP(Internet Control Message Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'TCP와 UDP의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '포트 스캐닝(Port Scanning)이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '방화벽(Firewall)이 무엇이며 왜 필요한가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '무선 네트워크 보안을 위한 주요 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'VLAN(Virtual LAN)이 무엇이며 어떻게 구성하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '서브넷 마스크(Subnet Mask)의 역할과 사용법에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '트래픽 분석을 위한 도구나 방법에 대해 알고 있나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'DDoS 공격에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '로드 밸런싱(Load Balancing)이 무엇이며 왜 필요한가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '서버 리스폰스 코드와 의미에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '네트워크 토폴로지(Network Topology)의 종류와 특징을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'ARP(Address Resolution Protocol)가 무엇이며 어떻게 작동하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '스니핑(Sniffing) 공격에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '서비스 역할 분리(Separation of Services)의 중요성은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '패킷 스니핑을 감지하고 방지하는 방법에 대해 알고 있나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '포트 포워딩과 포트 트리거링(Port Triggering)의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'SSH(Secure Shell)의 역할은 무엇이며 어떻게 작동하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '콘텐츠 딜리버리 네트워크(CDN)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'VLAN 간의 통신을 위한 라우터 설정 방법을 아나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '네트워크에서의 로그 관리와 분석의 중요성은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '네트워크 보안을 강화하기 위한 기술이나 방법을 아나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
---
+++ src/main/resources/sql/os-question.sql
@@ -0,0 +1,143 @@
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '운영체제(Operating System)가 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 1);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스와 스레드의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '멀티스레딩과 멀티프로세싱의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '데드락(Deadlock)이란 무엇인가요? 어떤 상황에서 발생하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스 스케줄링이 무엇인가요? 다양한 스케줄링 알고리즘에 대해 아나요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), 'CPU 스케줄러란 무엇이며 어떤 역할을 하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스의 상태를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '메모리 관리에서 메모리 할당과 해제의 과정을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '가상 메모리(Virtual Memory)가 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '페이지 교체 알고리즘에 대해 설명해주세요 (예: LRU, FIFO)', SUBSTRING(UUID(), 1, 12), 'OS', 5);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '파일 시스템이 무엇이며 어떤 역할을 하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '파일 액세스 방법에는 어떤 것들이 있나요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '디스크 스케줄링이 무엇이며 다양한 스케줄링 알고리즘에 대해 아나요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스 간 통신(IPC) 방법에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 5);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '운영체제의 커널(Kernel)이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '커널 모드와 사용자 모드의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '인터럽트(Interrupt)가 무엇이며 어떤 역할을 하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '시스템 호출(System Call)이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '컨텍스트 스위칭(Context Switching)이 무엇이며 어떤 상황에서 발생하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스 동기화가 왜 필요한가요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '세마포어(Semaphore)와 뮤텍스(Mutex)의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 5);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '스핀락(Spinlock)이란 무엇이며 어떤 상황에서 사용하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 5);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '교착 상태(Deadlock)를 방지하기 위한 방법에 대해 아나요?', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '스레드 동기화가 왜 필요한가요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '모니터(Monitor)가 무엇이며 어떤 역할을 하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '가상 머신(Virtual Machine)이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '컨테이너(Container)와 가상 머신(Virtual Machine)의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스 간 통신(IPC)에서 파이프(Pipe)와 메시지 큐(Message Queue)의 차이는 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '스레드 풀(Thread Pool)이 무엇이며 어떤 장점이 있나요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '메모리 단편화(Fragmentation)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '멀티태스킹과 멀티프로세싱의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '동적로딩(Dynamic Loading)과 동적연결(Dynamic Linking)이 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '시스템 로그와 애플리케이션 로그의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스 간 통신(IPC)에서 공유 메모리(Shared Memory)의 역할을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '페이징(Paging)과 세그멘테이션(Segmentation)의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 5);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스 간 통신(IPC)에서 소켓(Socket)의 역할을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '역순어셈블러(Disassembler)와 디버거(Debugger)의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 5);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '커널 패닉(Kernel Panic)이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '쓰레싱(Thrashing)이란 무엇이며 어떤 상황에서 발생하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '트랩(Trap)과 인터럽트(Interrupt)의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '파일 디스크립터(File Descriptor)가 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '스택 오버플로우(Stack Overflow)가 무엇이며 어떻게 발생하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '스와핑(Swapping)이 무엇이며 어떤 상황에서 발생하나요?', SUBSTRING(UUID(), 1, 12), 'OS', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), 'IPC 메커니즘 중에는 어떤 것들이 있나요? (예: 파이프, 메시지 큐)', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스의 우선순위를 어떻게 정할 수 있나요?', SUBSTRING(UUID(), 1, 12), 'OS', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '소프트웨어 인터럽트(Software Interrupt)와 하드웨어 인터럽트(Hardware Interrupt)의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '프로세스의 우선순위를 어떻게 조절할 수 있나요?', SUBSTRING(UUID(), 1, 12), 'OS', 4);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level) 
+VALUES (0, NOW(), NOW(), '멀티코어 프로세서에서 멀티스레드의 효과적인 활용 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'OS', 5);
