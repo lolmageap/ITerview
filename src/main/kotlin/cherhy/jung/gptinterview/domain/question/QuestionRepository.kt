156a2d0c46087f5d0671a39cecbd4afb2c6c417c
cherhy <ekxk1234@naver.com>
2023-10-26 23:29:46 +0900

    import Question Entity

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionRepository.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionRepository.kt
@@ -1,5 +1,6 @@
 package cherhy.jung.gptinterview.domain.question
 
+import cherhy.jung.gptinterview.domain.question.entity.Question
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface QuestionRepository: JpaRepository<Question, Long> {
