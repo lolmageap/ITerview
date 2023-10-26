f5764795fdd191faa9d99dca7bc6103f4d1d2001
cherhy <ekxk1234@naver.com>
2023-10-26 22:40:22 +0900

    add network data

--- src/main/resources/sql/network-question.sql
+++ src/main/resources/sql/network-question.sql
@@ -1,117 +1,119 @@
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'OSI 7계층에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), 'OSI 7계층 모델에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'TCP/IP 모델과 OSI 모델의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), 'TCP와 UDP의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'HTTP와 HTTPS의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+VALUES (0, NOW(), NOW(), 'IP 주소와 MAC 주소의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '서버와 클라이언트 간의 통신 과정을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+VALUES (0, NOW(), NOW(), '라우터와 스위치의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'IP 주소와 포트 번호의 역할에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+VALUES (0, NOW(), NOW(), 'DNS 서비스에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '라우팅이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+VALUES (0, NOW(), NOW(), '가상 사설 네트워크(VPN)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '서브넷이 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), '서브넷 마스크(Subnet Mask)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '서브넷팅(Subnetting)이 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), 'HTTP와 HTTPS의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'NAT(Network Address Translation)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+VALUES (0, NOW(), NOW(), '프록시 서버(Proxy Server)와 게이트웨이(Gateway)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'DNS의 역할과 작동 원리를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), '네트워크에서 사용되는 포트(Port)와 소켓(Socket)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'VPN(Virtual Private Network)이 무엇이며 어떻게 작동하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), 'TCP/IP 프로토콜 스택에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '라우터와 스위치의 차이점을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+VALUES (0, NOW(), NOW(), 'IPv4와 IPv6의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '포트 포워딩(Port Forwarding)이 무엇이며 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), 'DHCP(Dynamic Host Configuration Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'OSI 7계층 중 전송 계층(Transport Layer)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), 'CIDR(Classless Inter-Domain Routing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'OSI 7계층 중 네트워크 계층(Network Layer)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), '네트워크에서 패킷(Packet)과 프레임(Frame)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'OSI 7계층 중 데이터 링크 계층(Data Link Layer)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+VALUES (0, NOW(), NOW(), '라우팅 프로토콜에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'OSI 7계층 중 물리 계층(Physical Layer)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+VALUES (0, NOW(), NOW(), 'SSL/TLS에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'ARP(Address Resolution Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'NAT(Network Address Translation)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'TCP의 3-way Handshake에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'TCP의 4-way Termination에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'DDOS(Distributed Denial of Service) 공격에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '패킷 스니핑(Packet Sniffing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'HTTP 메소드(GET, POST, PUT, DELETE 등)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 1);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'FTP(File Transfer Protocol)와 SFTP(Secure File Transfer Protocol)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '쿠키(Cookie)와 세션(Session)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 1);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'CORS(Cross-Origin Resource Sharing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '웹소켓(WebSocket)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'REST와 SOAP의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '웹 서버와 애플리케이션 서버의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '로드 밸런싱(Load Balancing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'CDN(Content Delivery Network)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'MQTT 프로토콜에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'HTTP/2와 HTTP/3의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'HTTP 상태 코드에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 1);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '프라이빗 네트워크(Private Network)와 퍼블릭 네트워크(Public Network)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '라우팅 테이블(Routing Table)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'TCP의 흐름 제어(Flow Control)와 혼잡 제어(Congestion Control)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
+
+INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), '멀티캐스트(Multicast)와 유니캐스트(Unicast)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
 VALUES (0, NOW(), NOW(), 'ICMP(Internet Control Message Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'TCP와 UDP의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '포트 스캐닝(Port Scanning)이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '방화벽(Firewall)이 무엇이며 왜 필요한가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 2);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '무선 네트워크 보안을 위한 주요 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'VLAN(Virtual LAN)이 무엇이며 어떻게 구성하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '서브넷 마스크(Subnet Mask)의 역할과 사용법에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '트래픽 분석을 위한 도구나 방법에 대해 알고 있나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'DDoS 공격에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '로드 밸런싱(Load Balancing)이 무엇이며 왜 필요한가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '서버 리스폰스 코드와 의미에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '네트워크 토폴로지(Network Topology)의 종류와 특징을 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'ARP(Address Resolution Protocol)가 무엇이며 어떻게 작동하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '스니핑(Sniffing) 공격에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '서비스 역할 분리(Separation of Services)의 중요성은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '패킷 스니핑을 감지하고 방지하는 방법에 대해 알고 있나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 4);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '포트 포워딩과 포트 트리거링(Port Triggering)의 차이를 설명해주세요.', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'SSH(Secure Shell)의 역할은 무엇이며 어떻게 작동하나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '콘텐츠 딜리버리 네트워크(CDN)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'VLAN 간의 통신을 위한 라우터 설정 방법을 아나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '네트워크에서의 로그 관리와 분석의 중요성은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
-INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '네트워크 보안을 강화하기 위한 기술이나 방법을 아나요?', SUBSTRING(UUID(), 1, 12), 'NETWORK', 3);
-
