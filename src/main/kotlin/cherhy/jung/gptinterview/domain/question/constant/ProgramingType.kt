ea01cab962e7cd2a3b992efad76d83355866969c
lolmageap <ekxk1234@gmail.com>
2023-10-27 16:50:40 +0900

    change order

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/constant/ProgramingType.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/constant/ProgramingType.kt
@@ -2,10 +2,10 @@ package cherhy.jung.gptinterview.domain.question.constant
 
 // 1:1 == Question : ProgramingType
 enum class ProgramingType() {
-    JAVA,
-    C,
     `C++`,
     `C#`,
+    C,
+    JAVA,
     JAVASCRIPT,
     RUST,
     KOTLIN,
