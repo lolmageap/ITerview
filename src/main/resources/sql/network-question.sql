8c40323d0896bae2a423a86b2c753ccbf6989eb6
cherhy <ekxk1234@naver.com>
2023-10-27 22:38:42 +0900

    edit UUID length for token

--- src/main/resources/sql/network-question.sql
+++ src/main/resources/sql/network-question.sql
@@ -1,119 +1,119 @@
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'OSI 7계층 모델에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 1);
+VALUES (0, NOW(), NOW(), 'OSI 7계층 모델에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'TCP와 UDP의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'TCP와 UDP의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'IP 주소와 MAC 주소의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 1);
+VALUES (0, NOW(), NOW(), 'IP 주소와 MAC 주소의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '라우터와 스위치의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 1);
+VALUES (0, NOW(), NOW(), '라우터와 스위치의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'DNS 서비스에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 1);
+VALUES (0, NOW(), NOW(), 'DNS 서비스에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '가상 사설 네트워크(VPN)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '가상 사설 네트워크(VPN)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '서브넷 마스크(Subnet Mask)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '서브넷 마스크(Subnet Mask)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'HTTP와 HTTPS의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 1);
+VALUES (0, NOW(), NOW(), 'HTTP와 HTTPS의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '프록시 서버(Proxy Server)와 게이트웨이(Gateway)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '프록시 서버(Proxy Server)와 게이트웨이(Gateway)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '네트워크에서 사용되는 포트(Port)와 소켓(Socket)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '네트워크에서 사용되는 포트(Port)와 소켓(Socket)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'TCP/IP 프로토콜 스택에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'TCP/IP 프로토콜 스택에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'IPv4와 IPv6의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'IPv4와 IPv6의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'DHCP(Dynamic Host Configuration Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'DHCP(Dynamic Host Configuration Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'CIDR(Classless Inter-Domain Routing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), 'CIDR(Classless Inter-Domain Routing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '네트워크에서 패킷(Packet)과 프레임(Frame)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '네트워크에서 패킷(Packet)과 프레임(Frame)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '라우팅 프로토콜에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), '라우팅 프로토콜에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'SSL/TLS에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'SSL/TLS에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'ARP(Address Resolution Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'ARP(Address Resolution Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'NAT(Network Address Translation)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'NAT(Network Address Translation)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'TCP의 3-way Handshake에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'TCP의 3-way Handshake에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'TCP의 4-way Termination에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'TCP의 4-way Termination에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'DDOS(Distributed Denial of Service) 공격에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), 'DDOS(Distributed Denial of Service) 공격에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '패킷 스니핑(Packet Sniffing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), '패킷 스니핑(Packet Sniffing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'HTTP 메소드(GET, POST, PUT, DELETE 등)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 1);
+VALUES (0, NOW(), NOW(), 'HTTP 메소드(GET, POST, PUT, DELETE 등)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'FTP(File Transfer Protocol)와 SFTP(Secure File Transfer Protocol)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'FTP(File Transfer Protocol)와 SFTP(Secure File Transfer Protocol)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '쿠키(Cookie)와 세션(Session)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 1);
+VALUES (0, NOW(), NOW(), '쿠키(Cookie)와 세션(Session)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'CORS(Cross-Origin Resource Sharing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'CORS(Cross-Origin Resource Sharing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '웹소켓(WebSocket)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '웹소켓(WebSocket)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'REST와 SOAP의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'REST와 SOAP의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '웹 서버와 애플리케이션 서버의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '웹 서버와 애플리케이션 서버의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '로드 밸런싱(Load Balancing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), '로드 밸런싱(Load Balancing)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'CDN(Content Delivery Network)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), 'CDN(Content Delivery Network)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'MQTT 프로토콜에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), 'MQTT 프로토콜에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'HTTP/2와 HTTP/3의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), 'HTTP/2와 HTTP/3의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'HTTP 상태 코드에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 1);
+VALUES (0, NOW(), NOW(), 'HTTP 상태 코드에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '프라이빗 네트워크(Private Network)와 퍼블릭 네트워크(Public Network)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '프라이빗 네트워크(Private Network)와 퍼블릭 네트워크(Public Network)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '라우팅 테이블(Routing Table)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), '라우팅 테이블(Routing Table)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'TCP의 흐름 제어(Flow Control)와 혼잡 제어(Congestion Control)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), 'TCP의 흐름 제어(Flow Control)와 혼잡 제어(Congestion Control)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '멀티캐스트(Multicast)와 유니캐스트(Unicast)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 2, 2);
+VALUES (0, NOW(), NOW(), '멀티캐스트(Multicast)와 유니캐스트(Unicast)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 2, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'ICMP(Internet Control Message Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 2, 3);
+VALUES (0, NOW(), NOW(), 'ICMP(Internet Control Message Protocol)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 2, 3);
--- src/main/resources/sql/vcs-question.sql
+++ src/main/resources/sql/vcs-question.sql
@@ -1,59 +1,59 @@
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '버전 관리 시스템(Version Control System)이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 1);
+VALUES (0, NOW(), NOW(), '버전 관리 시스템(Version Control System)이란 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git과 SVN의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'Git과 SVN의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git의 기본 작업 흐름에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 7, 1);
+VALUES (0, NOW(), NOW(), 'Git의 기본 작업 흐름에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 7, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git에서 브랜치(Branch)란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 1);
+VALUES (0, NOW(), NOW(), 'Git에서 브랜치(Branch)란 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git에서 Merge와 Rebase의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'Git에서 Merge와 Rebase의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'CI(Continuous Integration)란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'CI(Continuous Integration)란 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'CD(Continuous Deployment)와 CD(Continuous Delivery)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 3);
+VALUES (0, NOW(), NOW(), 'CD(Continuous Deployment)와 CD(Continuous Delivery)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Jenkins의 사용법과 장점에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'Jenkins의 사용법과 장점에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git에서 충돌(Conflict)이 발생했을 때, 어떻게 해결하나요?', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'Git에서 충돌(Conflict)이 발생했을 때, 어떻게 해결하나요?', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '형상 관리 도구(Configuration Management Tools)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 1);
+VALUES (0, NOW(), NOW(), '형상 관리 도구(Configuration Management Tools)의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Docker를 사용하는 이유와 장점에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'Docker를 사용하는 이유와 장점에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Ansible의 특징과 사용법에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 7, 3);
+VALUES (0, NOW(), NOW(), 'Ansible의 특징과 사용법에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 7, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git에서 Cherry-pick이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'Git에서 Cherry-pick이란 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git의 Stash 기능에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'Git의 Stash 기능에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '형상 관리에서 효과적인 브랜치 전략(Branch Strategy)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 7, 3);
+VALUES (0, NOW(), NOW(), '형상 관리에서 효과적인 브랜치 전략(Branch Strategy)에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 7, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git에서 Squash란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), 'Git에서 Squash란 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 2);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '버전 관리에서 Tagging이란 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 1);
+VALUES (0, NOW(), NOW(), '버전 관리에서 Tagging이란 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 1);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Git Flow와 GitHub Flow의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 3);
+VALUES (0, NOW(), NOW(), 'Git Flow와 GitHub Flow의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), 'Terraform의 역할과 장점에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 12), 7, 3);
+VALUES (0, NOW(), NOW(), 'Terraform의 역할과 장점에 대해 설명해주세요.', SUBSTRING(UUID(), 1, 8), 7, 3);
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_menu_id, level)
-VALUES (0, NOW(), NOW(), '형상관리에서 롤백(Rollback)과 롤포워드(Rollforward)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 12), 7, 2);
+VALUES (0, NOW(), NOW(), '형상관리에서 롤백(Rollback)과 롤포워드(Rollforward)의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 7, 2);
