555619dcd30411f67e6c9d3d6f7321f993f9ed0e
lolmageap <ekxk1234@naver.com>
2023-10-29 21:29:12 +0900

    rename findRandomQuestion method

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionRepository.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionRepository.kt
@@ -4,9 +4,9 @@ import cherhy.jung.gptinterview.domain.question.entity.Question
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
-interface QuestionRepository: JpaRepository<Question, Long> {
+interface QuestionRepository: JpaRepository<Question, Long>, QuestionRepositoryCustom {
 
     @Query(value = "SELECT q FROM Question q ORDER BY RAND() LIMIT 1")
-    fun findRandomQuestion(): Question
+    fun findAllOrderByRandom(): Question
 
 }
