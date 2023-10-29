54d22219aa2f2f8a8f47035de902d2edda9fd7db
cherhy <ekxk1234@naver.com>
2023-10-29 09:26:53 +0900

    remove class

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionMenuRepository.kt
+++
@@ -1,7 +0,0 @@
-package cherhy.jung.gptinterview.domain.question
-
-import cherhy.jung.gptinterview.domain.question.entity.QuestionMenu
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface QuestionMenuRepository: JpaRepository<QuestionMenu, Long> {
-}
