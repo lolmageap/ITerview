5a704fa05c872d4bd2da2d36bfc46ec7df954014
cherhy <ekxk1234@naver.com>
2023-10-29 10:26:15 +0900

    create question dto in service layer

---
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/dto/QuestionResponseS.kt
@@ -0,0 +1,20 @@
+package cherhy.jung.gptinterview.domain.question.dto
+
+import cherhy.jung.gptinterview.domain.question.constant.QuestionType
+import cherhy.jung.gptinterview.domain.question.entity.Question
+
+data class QuestionResponseS(
+    val token: String,
+    val detail: String,
+    val type: QuestionType,
+) {
+    companion object {
+        fun of(question: Question): QuestionResponseS {
+            return QuestionResponseS(
+                token = question.token,
+                detail = question.title,
+                type = question.questionType,
+            )
+        }
+    }
+}
