86adc0d17e2975a9a65976995a4609529c0992c3
cherhy <ekxk1234@naver.com>
2023-10-29 09:29:43 +0900

    create api test

--- src/main/kotlin/cherhy/jung/gptinterview/controller/QuestionController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/QuestionController.kt
@@ -1,7 +1,10 @@
 package cherhy.jung.gptinterview.controller
 
+import cherhy.jung.gptinterview.domain.position.PositionType
 import cherhy.jung.gptinterview.domain.question.QuestionReadService
+import cherhy.jung.gptinterview.domain.question.constant.QuestionType
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -12,6 +15,12 @@ class QuestionController(
 ) {
 
     @GetMapping
-    fun get() = questionReadService.getQuestion()
+    fun getRandomQuestion() = questionReadService.getQuestion()
+
+    @GetMapping("/{question-type}")
+    fun getQuestionByQuestionType(@PathVariable(name = "question-type") questionType: QuestionType) = "질문타입"
+
+    @GetMapping("/position/{position-type}")
+    fun getQuestionByPositionType(@PathVariable(name = "position-type") positionType: PositionType) = "포지션타입"
 
 }
