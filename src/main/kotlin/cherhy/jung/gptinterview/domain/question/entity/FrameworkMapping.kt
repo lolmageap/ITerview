1e20b65daf3838b678e0d2b214d8b150747491cc
cherhy <ekxk1234@naver.com>
2023-10-29 09:28:16 +0900

    foreign key setting

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/entity/FrameworkMapping.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/entity/FrameworkMapping.kt
@@ -1,17 +1,17 @@
 package cherhy.jung.gptinterview.domain.question.entity
 
 import cherhy.jung.gptinterview.util.BaseEntity
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.ManyToOne
+import jakarta.persistence.*
 
 @Entity
 class FrameworkMapping(
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
     val question: Question,
 
     @ManyToOne(fetch = FetchType.LAZY)
-    val frameworkMapping: FrameworkMapping,
+    @JoinColumn(foreignKey = ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    val frameworkMenu: FrameworkMenu,
 
 ) : BaseEntity()
