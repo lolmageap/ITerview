631213dab670b2ff1fcd99094c3df26822eeb36b
cherhy <ekxk1234@naver.com>
2023-10-29 09:28:42 +0900

    edit relationship

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/entity/Question.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/entity/Question.kt
@@ -1,13 +1,17 @@
 package cherhy.jung.gptinterview.domain.question.entity
 
+import cherhy.jung.gptinterview.domain.question.constant.QuestionType
 import cherhy.jung.gptinterview.util.BaseEntity
 import jakarta.persistence.Entity
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
 
 @Entity
 class Question(
     val title: String,
 
-    val questionMenuId: Long,
+    @Enumerated(EnumType.STRING)
+    val questionType: QuestionType,
 
     val token: String,
 
