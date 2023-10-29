3bb60c07946f108d96fcbfd5b8bacb62d01694e9
cherhy <ekxk1234@naver.com>
2023-10-29 10:26:38 +0900

    create a get random question

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionReadService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionReadService.kt
@@ -1,13 +1,16 @@
 package cherhy.jung.gptinterview.domain.question
 
 import cherhy.jung.gptinterview.annotation.ReadService
+import cherhy.jung.gptinterview.domain.question.dto.QuestionResponseS
 
 @ReadService
 class QuestionReadService(
     private val questionRepository: QuestionRepository,
     ) {
 
-    fun getQuestion() {}
+    fun getQuestion(): QuestionResponseS =
+        questionRepository.findRandomQuestion()
+        .let(QuestionResponseS::of)
 
 }
 
--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionRepository.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionRepository.kt
@@ -2,7 +2,11 @@ package cherhy.jung.gptinterview.domain.question
 
 import cherhy.jung.gptinterview.domain.question.entity.Question
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface QuestionRepository: JpaRepository<Question, Long> {
+
+    @Query(value = "SELECT q FROM Question q ORDER BY RAND() LIMIT 1")
+    fun findRandomQuestion(): Question
 
 }
