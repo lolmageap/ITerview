ab8b00bf6276dde625e341e2478871b01cc39a11
cherhy <ekxk1234@naver.com>
2023-10-30 23:00:45 +0900

    edit firstNotNullOf -> firstNotNullOfOrNull and exception handling

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionReadService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionReadService.kt
@@ -5,33 +5,38 @@ import cherhy.jung.gptinterview.domain.question.constant.FrameworkType
 import cherhy.jung.gptinterview.domain.question.constant.ProgramingType
 import cherhy.jung.gptinterview.domain.question.constant.QuestionType
 import cherhy.jung.gptinterview.domain.question.dto.QuestionResponseS
+import cherhy.jung.gptinterview.exception.DomainName.*
+import cherhy.jung.gptinterview.exception.NotFoundException
 
 @ReadService
 class QuestionReadService(
     private val questionRepository: QuestionRepository,
-    ) {
+) {
 
-    fun getQuestion(): QuestionResponseS =
-        questionRepository.findAllOrderByRandom()
-            .let(QuestionResponseS::of)
+    fun getQuestion(questionTokens: MutableList<String>): QuestionResponseS =
+        questionRepository.findTopByRandom(questionTokens)
+            ?.let(QuestionResponseS::of)
+            ?: throw NotFoundException(QUESTION)
 
-    fun getQuestion(questionType: QuestionType): QuestionResponseS =
-        questionRepository.findByQuestionType(questionType)
-            .firstNotNullOf {
+    fun getQuestion(questionType: QuestionType, questionTokens: MutableList<String>): QuestionResponseS =
+        questionRepository.findByQuestionType(questionType, questionTokens)
+            .firstNotNullOfOrNull {
                 it.let(QuestionResponseS::of)
             }
+            ?: throw NotFoundException(QUESTION)
 
-    fun getQuestion(programingType: ProgramingType): QuestionResponseS =
-        questionRepository.findByProgramingType(programingType)
-            .firstNotNullOf {
+    fun getQuestion(programingType: ProgramingType, questionTokens: MutableList<String>): QuestionResponseS =
+        questionRepository.findByProgramingType(programingType, questionTokens)
+            .firstNotNullOfOrNull {
                 it.let(QuestionResponseS::of)
             }
+            ?: throw NotFoundException(QUESTION)
 
-    fun getQuestion(frameworkType: FrameworkType): QuestionResponseS =
-        questionRepository.findByFrameworkType(frameworkType)
-            .firstNotNullOf {
+    fun getQuestion(frameworkType: FrameworkType, questionTokens: MutableList<String>): QuestionResponseS =
+        questionRepository.findByFrameworkType(frameworkType, questionTokens)
+            .firstNotNullOfOrNull {
                 it.let(QuestionResponseS::of)
             }
-
+            ?: throw NotFoundException(QUESTION)
 }
 
