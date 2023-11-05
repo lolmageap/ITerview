3aaf4adb4ada27254ebf139efa8e7819cdbe1da2
lolmageap <ekxk1234@naver.com>
2023-11-05 10:41:22 +0900

    edit default method listOf -> emptyList

--- src/main/kotlin/cherhy/jung/gptinterview/domain/position/PositionDetails.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/position/PositionDetails.kt
@@ -6,67 +6,67 @@ import cherhy.jung.gptinterview.domain.question.constant.QuestionType
 
 class Gamer(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class DataAnalysis(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class DataEngineer(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class BackendDeveloper(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class FrontendDeveloper(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class InformationSecurity(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class AndroidDeveloper(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class IosDeveloper(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class AiDeveloper(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class BlockchainDeveloper(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
 class DatabaseAdministrator(
     override val questionTypes: List<QuestionType>,
-    override val frameworkTypes: List<FrameworkType> = listOf(),
-    override val programingTypes: List<ProgramingType> = listOf(),
+    override val frameworkTypes: List<FrameworkType> = emptyList(),
+    override val programingTypes: List<ProgramingType> = emptyList(),
 ) : Position()
 
