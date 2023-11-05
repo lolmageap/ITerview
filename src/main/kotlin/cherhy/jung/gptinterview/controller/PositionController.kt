7628010f55b54bb4422c85954ac2864d7f80e7a6
lolmageap <ekxk1234@naver.com>
2023-11-05 10:44:20 +0900

    edit get position detail API and add description

--- src/main/kotlin/cherhy/jung/gptinterview/controller/PositionController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/PositionController.kt
@@ -2,23 +2,30 @@ package cherhy.jung.gptinterview.controller
 
 import cherhy.jung.gptinterview.domain.position.Position
 import cherhy.jung.gptinterview.domain.position.PositionType
-import cherhy.jung.gptinterview.domain.question.QuestionReadService
 import io.swagger.v3.oas.annotations.Operation
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/position")
-class PositionController(
-    private val questionReadService: QuestionReadService,
-) {
+class PositionController {
 
     @Operation(summary = "포지션 정보", description = "요청한 포지션의 상세 스펙에 대해 조회한다.")
-    @GetMapping("/{position-type}")
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     fun getPosition(
-        @PathVariable(name = "position-type") positionType: PositionType,
-    ) = Position.from(positionType)
-        .let(PositionResponse::of)
+        @RequestParam positionTypes: List<PositionType>,
+    ) =
+        positionTypes
+            .map(Position::of)
+            .let { positions ->
+                PositionResponse.of(positions)
+            }
+//        positionTypes
+//            .map(Position::of)
+//            .let(PositionResponse::of)
 }
