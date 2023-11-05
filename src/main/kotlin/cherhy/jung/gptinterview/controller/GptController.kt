fd14e06c80d631e6ee72df09346b75480c3e265d
lolmageap <ekxk1234@naver.com>
2023-11-05 10:40:48 +0900

    add authorization setting

--- src/main/kotlin/cherhy/jung/gptinterview/controller/GptController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/GptController.kt
@@ -1,8 +1,11 @@
 package cherhy.jung.gptinterview.controller
 
 import cherhy.jung.gptinterview.dependency.GptApi
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -11,6 +14,8 @@ class GptController(
     ) {
 
     @PostMapping("/")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.CREATED)
     fun postAndGet(@RequestBody question: String) = gptApi.generateText(question)
 
 }
