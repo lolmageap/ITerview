5edfb5a8a6f9937f2ad67ab527ea38edc4ed93fb
lolmageap <ekxk1234@naver.com>
2023-11-05 10:37:27 +0900

    remove position menu

--- src/main/kotlin/cherhy/jung/gptinterview/domain/position/PositionRepository.kt
+++
@@ -1,6 +0,0 @@
-package cherhy.jung.gptinterview.domain.position
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface PositionRepository: JpaRepository<PositionMenu, Long> {
-}
