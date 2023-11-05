a5f725b24ca8a0a3106d3547ec99767509a4502f
lolmageap <ekxk1234@naver.com>
2023-11-05 10:48:31 +0900

    edit factory method : parameter type and return type

--- src/main/kotlin/cherhy/jung/gptinterview/controller/PositionDTO.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/PositionDTO.kt
@@ -3,19 +3,21 @@ package cherhy.jung.gptinterview.controller
 import cherhy.jung.gptinterview.domain.position.Position
 import cherhy.jung.gptinterview.domain.question.constant.FrameworkType
 import cherhy.jung.gptinterview.domain.question.constant.ProgramingType
+import cherhy.jung.gptinterview.domain.question.constant.QuestionLevel
 import cherhy.jung.gptinterview.domain.question.constant.QuestionType
 
 data class PositionResponse(
-    val questionTypes: List<QuestionType>,
-    val frameworkTypes: List<FrameworkType>,
-    val programingTypes: List<ProgramingType>,
+    val questionTypes: Set<QuestionType>,
+    val frameworkTypes: Set<FrameworkType>,
+    val programingTypes: Set<ProgramingType>,
+    val levels: Set<QuestionLevel> = emptySet(),
 ) {
     companion object {
-        fun of(position: Position): PositionResponse =
+        fun of(positions: List<Position>): PositionResponse =
             PositionResponse(
-                questionTypes = position.questionTypes,
-                frameworkTypes = position.frameworkTypes,
-                programingTypes = position.programingTypes,
+                questionTypes = positions.flatMap { it.questionTypes }.toSet(),
+                frameworkTypes = positions.flatMap { it.frameworkTypes  }.toSet(),
+                programingTypes = positions.flatMap { it.programingTypes  }.toSet(),
             )
     }
 }
