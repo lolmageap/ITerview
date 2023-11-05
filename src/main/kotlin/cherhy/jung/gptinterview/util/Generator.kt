f660a862e5edf834ef93c4b098c465b01edaf891
lolmageap <ekxk1234@naver.com>
2023-11-05 11:08:17 +0900

    create generate question to gpt

--- src/main/kotlin/cherhy/jung/gptinterview/util/Generator.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/util/Generator.kt
@@ -11,8 +11,24 @@ object Generator {
         return "$now-$uuid"
     }
 
-    fun generateSalt(): String {
-        return UUID.randomUUID().toString().replace("-", "")
-    }
+    fun generateSalt(): String =
+        UUID.randomUUID().toString().replace("-", "")
+
+
+    fun generateQuestionToGpt(question: String, answer: String): String =
+        """
+            질문 : $question
+            
+            답안 : $answer
+            
+            위 질문과 답안에 대해 채점을 해서 점수를 매겨줘
+            그리고 아래 양식에 맞게 점수와 피드백을 작성해줘
+            
+            양식 {
+                점수 : 
+                피드백 : 
+            }
+        """.trimIndent()
+
 
 }
