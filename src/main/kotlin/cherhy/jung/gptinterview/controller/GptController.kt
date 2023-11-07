99da02e0a1affc7f6ea5e1a0308c51c69b0208b6
cherhy <ekxk1234@naver.com>
2023-11-07 21:22:44 +0900

    create answer api

--- src/main/kotlin/cherhy/jung/gptinterview/controller/GptController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/GptController.kt
@@ -1,21 +1,29 @@
 package cherhy.jung.gptinterview.controller
 
-import cherhy.jung.gptinterview.dependency.GptApi
+import cherhy.jung.gptinterview.domain.customer.AuthCustomer
+import cherhy.jung.gptinterview.usecase.GptAnswerUseCase
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@RequestMapping("/answer")
 class GptController(
-    private val gptApi: GptApi,
-    ) {
+    private val gptAnswerUseCase: GptAnswerUseCase,
+) {
 
-    @PostMapping("/")
+    @PostMapping
     @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.CREATED)
-    fun postAndGet(@RequestBody question: String) = gptApi.generateText(question)
+    fun postAndGet(
+        @RequestBody gptRequest: GptRequest,
+        @AuthenticationPrincipal authCustomer: AuthCustomer,
+    ): String =
+        gptAnswerUseCase.requestAnswerToGpt(authCustomer.customerId, gptRequest)
 
 }
