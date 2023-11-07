c47717ac603194c840692a667b9af77a2de32261
cherhy <ekxk1234@naver.com>
2023-11-07 21:24:04 +0900

    edit gpt model "text-davinci-003" -> "gpt-3.5-turbo-instruct"

--- src/main/kotlin/cherhy/jung/gptinterview/domain/gpt/GptApi.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/gpt/GptApi.kt
@@ -1,5 +1,6 @@
-package cherhy.jung.gptinterview.dependency
+package cherhy.jung.gptinterview.domain.gpt
 
+import cherhy.jung.gptinterview.util.log
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
@@ -23,13 +24,13 @@ class GptApi(
 
 //    temperature 는 생성된 텍스트의 다양성을 조절
 //    max tokens 는 생성된 텍스트의 최대 길이를 제한
-    fun generateText(prompt: String): String? {
+    fun generateText(prompt: String): String {
         val headers = HttpHeaders()
         headers.contentType = MediaType.APPLICATION_JSON
         headers.set("Authorization", "Bearer $API_KEY")
 
         val requestBody: MutableMap<String, Any> = HashMap()
-        requestBody["model"] = "text-davinci-003"
+        requestBody["model"] = "gpt-3.5-turbo-instruct"
         requestBody["prompt"] = prompt
         requestBody["temperature"] = TEMPERATURE
         requestBody["max_tokens"] = MAX_TOKENS
