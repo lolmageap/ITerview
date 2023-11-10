9c1d62401fa732455ecb46e0505a1f165bc44116
cherhy <ekxk1234@naver.com>
2023-11-10 22:52:30 +0900

    create SignUpRequestTest

---
+++ src/test/kotlin/cherhy/jung/gptinterview/controller/SignUpRequestTest.kt
@@ -0,0 +1,18 @@
+package cherhy.jung.gptinterview.controller
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.core.spec.style.StringSpec
+
+class SignUpRequestTest : StringSpec() {
+    init {
+        "회원이 입력한 비밀번호와 비밀번호 재입력이 서로 다르면 에러를 발생한다." {
+            shouldThrow<IllegalArgumentException> {
+                SignUpRequest(
+                    email = "ekxk1234@naver.com",
+                    password = "abcd1234",
+                    confirmPassword = "test1234",
+                )
+            }
+        }
+    }
+}
