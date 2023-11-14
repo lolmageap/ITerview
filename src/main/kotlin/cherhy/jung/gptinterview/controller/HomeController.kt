965f04cd30428da5430acee5768b5ecab404d959
cherhy <ekxk1234@naver.com>
2023-11-14 23:50:17 +0900

    create home controller

---
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/HomeController.kt
@@ -0,0 +1,25 @@
+package cherhy.jung.gptinterview.controller
+
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+
+@Tag(name = "메인")
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    fun home(): String = "index.html"
+
+    @GetMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    fun login(): String = "login.html"
+
+    @GetMapping("/sign-up")
+    @ResponseStatus(HttpStatus.OK)
+    fun signup(): String = "signup.html"
+
+}
