d18bf53aff2bea64d4188cf5daad6b42b5d285e3
cherhy <ekxk1234@naver.com>
2023-11-16 23:40:29 +0900

    add validation and checkbox method

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -2,28 +2,60 @@ const login = async () => {
     const email = document.getElementById('loginId').value
     const password = document.getElementById('loginPassword').value
 
+    if (!email || !password) {
+        alert("모든 필수 항목을 입력해주세요.");
+        return;
+    }
+
     const response = await fetch("/authority/sign-in", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({'email': email, 'password': password})
     });
 
-    const authorization = response.headers.get("Authorization")
-
-    if (authorization) {
-        document.cookie = `Authorization=${authorization}; path=/`;
-    } else {
-        console.error("Authorization 헤더가 응답에 없습니다. 로그인에 실패했을 수 있습니다.");
-    }
+    await setCookie(response);
 }
 
 const signup = async () => {
     const email = document.getElementById('signupId').value
     const password = document.getElementById('signupPassword').value
     const confirmPassword = document.getElementById('confirmPassword').value
+
+    if (!email || !password || !confirmPassword) {
+        alert("모든 필수 항목을 입력해주세요.");
+        return;
+    }
+
     const response = await fetch("/authority/sign-up", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({'email': email, 'password': password, 'confirmPassword': confirmPassword})
     });
+
+    await setCookie(response);
+}
+
+const setCookie = async response => {
+    if (response.status == 201) {
+        const authorization = response.headers.get("Authorization")
+        if (authorization) {
+            document.cookie = `Authorization=${authorization}; path=/`;
+            location.href = '/'
+        } else {
+            console.error("Authorization 헤더가 응답에 없습니다. 로그인에 실패했을 수 있습니다.");
+        }
+    } else {
+        let data = await response.json();
+        alert(data.message)
+    }
+}
+
+const check = () => {
+    const checkbox = document.getElementById('reg-log');
+    checkbox.checked = true;
+}
+
+const uncheck = () => {
+    const checkbox = document.getElementById('reg-log');
+    checkbox.checked = false;
 }
