ad67769154a0673d3897fd109066cfef179241f6
lolmageap <ekxk1234@gmail.com>
2023-11-16 19:46:32 +0900

    add Enum LOGIN

--- src/main/kotlin/cherhy/jung/gptinterview/exception/DomainName.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/exception/DomainName.kt
@@ -1,5 +1,5 @@
 package cherhy.jung.gptinterview.exception
 
 enum class DomainName(val value: String) {
-    CUSTOMER("회원"), QUESTION("질문")
+    CUSTOMER("회원"), QUESTION("질문"), LOGIN("로그인 해주세요")
 }
