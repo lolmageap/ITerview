78c485c5b95010155a6ae9522f79527280f77977
lolmageap <ekxk1234@gmail.com>
2023-11-20 17:56:59 +0900

    add chat container event

--- src/main/resources/static/js/chat.js
+++ src/main/resources/static/js/chat.js
@@ -1,8 +1,33 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const checkbox = document.getElementById('checkbox');
-    const chatContainer = document.querySelector('.chat-container');
+document.addEventListener('DOMContentLoaded', () => {
+    const checkbox = document.getElementById('checkbox')
+    const submit = document.getElementById('position-submit')
 
     checkbox.addEventListener('change', function () {
-        chatContainer.style.opacity = checkbox.checked ? 0 : 1;
-    });
-});
+        toggleChatContainer(checkbox);
+    })
+
+    submit.addEventListener('click', function () {
+        toggleChatContainer(checkbox)
+    })
+})
+
+const toggleChatContainer = checkbox => {
+    const chatContainer = document.querySelector('.chat-container');
+
+    chatContainer.style.opacity = checkbox.checked ? 0 : 1;
+    chatContainer.style.visibility = checkbox.checked ? 'hidden' : 'visible';
+
+    const transitionEndHandler = () => {
+        chatContainer.style.transition = 'none';
+        chatContainer.removeEventListener('transitionend', transitionEndHandler);
+
+        chatContainer.style.visibility = checkbox.checked ? 'hidden' : 'visible';
+    };
+
+    chatContainer.addEventListener('transitionend', transitionEndHandler);
+
+    chatContainer.offsetHeight;
+
+    chatContainer.style.opacity = checkbox.checked ? 0 : 1;
+};
+
