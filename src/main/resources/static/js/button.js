5154954476bba7c8200484be431babc21c8105a6
cherhy <ekxk1234@naver.com>
2023-11-21 00:13:08 +0900

    add request server method

--- src/main/resources/static/js/button.js
+++ src/main/resources/static/js/button.js
@@ -1,25 +1,50 @@
-const handleSubmit = () => {
-    const inputField = document.getElementById('inputField');
-    const submitBtn = document.getElementById('submitBtn');
-    const sendBtn = document.getElementById('sendBtn');
+const handleSubmit = async () => {
+    const inputField = document.getElementById('inputField')
+    const submitBtn = document.getElementById('submitBtn')
+    const sendBtn = document.getElementById('sendBtn')
 
     if (inputField.value.trim() !== '') {
-        inputField.style.display = 'none';
-        submitBtn.style.display = 'none';
+        inputField.style.display = 'none'
+        submitBtn.style.display = 'none'
 
-        sendBtn.style.display = 'inline-block';
+        sendBtn.style.display = 'inline-block'
     }
 }
 
-const handleSend = () => {
-    const inputField = document.getElementById('inputField');
-    const submitBtn = document.getElementById('submitBtn');
-    const sendBtn = document.getElementById('sendBtn');
+const handleSend = async () => {
+    const inputField = document.getElementById('inputField')
+    const submitBtn = document.getElementById('submitBtn')
+    const sendBtn = document.getElementById('sendBtn')
 
-    sendBtn.style.display = 'none';
+    sendBtn.style.display = 'none'
 
-    inputField.style.display = 'inline-block';
-    submitBtn.style.display = 'inline-block';
+    inputField.style.display = 'inline-block'
+    submitBtn.style.display = 'inline-block'
 
-    inputField.value = '';
+    inputField.value = ''
+    const questions = makeRequestParams(questionTypes, "questionTypes")
+    const programing = makeRequestParams(programingTypes, "programingTypes")
+    const frameworks = makeRequestParams(frameworkTypes, "frameworkTypes")
+
+    const queryParams = [questions, programing, frameworks]
+        .map(param => param === '' ? param : param + '&')
+        .join('')
+        .slice(0, -1)
+
+    if (getCookie() !== '') {
+        const url = "/question" + (queryParams !== '' ? '?' + queryParams : '')
+
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": getCookie()
+            },
+        })
+
+        const json = response.json()
+        if (response.status == 403 || response.status == 401) {
+            location.href = '/login'
+        }
+    }
 }
