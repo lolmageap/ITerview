a41da6cb152c15f9e34fb347aebace02dd761218
lolmageap <ekxk1234@gmail.com>
2023-11-21 18:44:29 +0900

    add level functions

--- src/main/resources/static/js/menu.js
+++ src/main/resources/static/js/menu.js
@@ -2,17 +2,20 @@ const positions = new Set()
 const questionTypes = new Set()
 const programingTypes = new Set()
 const frameworkTypes = new Set()
+const levels = new Set()
 
 const closeTab = () => {
     const checkbox = document.getElementById('checkbox')
     if (!checkbox.checked) {
         closePosition()
         closeCategory()
+        closeLevel()
         closePrograming()
         closeFramework()
         removeAllTypes()
         document.getElementById('cbx-1').checked = false
         document.getElementById('cbx-14').checked = false
+        document.getElementById('cbx-47').checked = false
     }
 }
 
@@ -43,6 +46,13 @@ const closeCategory = () => {
     uncheckChildren('category-detail')
 }
 
+const closeLevel = () => {
+    const list = document.getElementById('level-detail')
+    list.style.display = 'none'
+    uncheckChildren('level-detail')
+    levels.clear()
+}
+
 const showCategoryAndCheck = () => {
     showCategory()
     toggleCheckbox('cbx-14')
@@ -66,6 +76,13 @@ const showCategory = () => {
     }
 }
 
+const openProgramingAndFramework = () => {
+    const framework = document.getElementById('framework-detail')
+    const programing = document.getElementById('programing-detail')
+    framework.style.display = 'flex'
+    programing.style.display = 'flex'
+}
+
 const showProgramingAndCheck = () => {
     showPrograming()
     toggleCheckbox('cbx-22')
@@ -76,12 +93,18 @@ const showFrameworkAndCheck = () => {
     toggleCheckbox('cbx-23')
 }
 
+const showLevelAndCheck = () => {
+    showLevel()
+    toggleCheckbox('cbx-47')
+}
+
 const showPrograming = () => {
     const list = document.getElementById('programing-detail')
     if (list.style.display === '' || list.style.display === 'none') {
         list.style.display = 'flex'
     } else {
-        uncheckChildren('programing-detail')
+        closePrograming()
+        programingTypes.clear()
     }
 }
 
@@ -96,7 +119,16 @@ const showFramework = () => {
     if (list.style.display === '' || list.style.display === 'none') {
         list.style.display = 'flex'
     } else {
-        uncheckChildren('framework-detail')
+        closeFramework()
+    }
+}
+
+const showLevel = () => {
+    const list = document.getElementById('level-detail')
+    if (list.style.display === '' || list.style.display === 'none') {
+        list.style.display = 'flex'
+    } else {
+        closeLevel()
     }
 }
 
@@ -104,6 +136,7 @@ const closeFramework = () => {
     const list = document.getElementById('framework-detail')
     list.style.display = 'none'
     uncheckChildren('framework-detail')
+    frameworkTypes.clear()
 }
 
 const toggleCheckbox = id => {
@@ -139,8 +172,7 @@ const addPositionInSet = id => {
     }
 
     openCategory()
-    showPrograming()
-    showFramework()
+    openProgramingAndFramework()
     removeAllTypes()
     sendPositionToServer()
 }
@@ -195,4 +227,61 @@ const removeAllTypes = () => {
     questionTypes.clear()
     programingTypes.clear()
     frameworkTypes.clear()
+    levels.clear()
+}
+
+const addSetToQuestionAndToggle = (box, id) => {
+    toggleCheckbox(id)
+    addSetToQuestion(box)
+}
+
+const addSetToQuestion = box => {
+    const name = box.getAttribute("name");
+    if (questionTypes.has(name)) {
+        questionTypes.delete(name)
+    } else {
+        questionTypes.add(name)
+    }
+}
+
+const addSetToProgramingAndToggle = (box, id) => {
+    toggleCheckbox(id)
+    addSetToPrograming(box)
+}
+
+const addSetToPrograming = box => {
+    const name = box.getAttribute("name");
+    if (programingTypes.has(name)) {
+        programingTypes.delete(name)
+    } else {
+        programingTypes.add(name)
+    }
+}
+
+const addSetToFrameworkAndToggle = (box, id) => {
+    toggleCheckbox(id)
+    addSetToFramework(box)
+}
+
+const addSetToFramework = box => {
+    const name = box.getAttribute("name");
+    if (frameworkTypes.has(name)) {
+        frameworkTypes.delete(name)
+    } else {
+        frameworkTypes.add(name)
+    }
+}
+
+const addSetToLevelAndToggle = (box, id) => {
+    toggleCheckbox(id)
+    addSetToLevel(box)
+}
+
+const addSetToLevel = box => {
+    const name = box.getAttribute("name");
+    if (levels.has(name)) {
+        levels.delete(name)
+    } else {
+        levels.add(name)
+    }
 }
