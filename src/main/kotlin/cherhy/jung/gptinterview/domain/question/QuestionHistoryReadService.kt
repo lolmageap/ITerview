a21a334bc36acf2585b54a7cce0bf8f9399a37ed
cherhy <ekxk1234@naver.com>
2023-11-23 23:54:34 +0900

    add get questions method

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryReadService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryReadService.kt
@@ -1,11 +1,21 @@
 package cherhy.jung.gptinterview.domain.question
 
 import cherhy.jung.gptinterview.annotation.ReadService
+import cherhy.jung.gptinterview.domain.question.dto.QuestionHistoryResponseS
+import org.springframework.data.domain.Pageable
 
 @ReadService
 class QuestionHistoryReadService(
     private val questionHistoryRepository: QuestionHistoryRepository,
 ) {
+
+    fun getAllQuestionHistories(customerId: Long, pageable: Pageable): List<QuestionHistoryResponseS> =
+        questionHistoryRepository.findAllByCustomerId(customerId, pageable)
+            .map(QuestionHistoryResponseS::of)
+
+    fun getQuestionHistory(customerId: Long, token: String): QuestionHistoryResponseS =
+        questionHistoryRepository.findByCustomerIdAndToken(customerId, token)
+            .let(QuestionHistoryResponseS::of)
 
 }
 
--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryRepositoryCustom.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryRepositoryCustom.kt
@@ -1,18 +1,62 @@
 package cherhy.jung.gptinterview.domain.question
 
-import cherhy.jung.gptinterview.domain.question.entity.Question
+import cherhy.jung.gptinterview.domain.question.dto.QuestionHistoryResponseR
+import cherhy.jung.gptinterview.domain.question.entity.QQuestion.question
+import cherhy.jung.gptinterview.domain.question.entity.QQuestionHistory.questionHistory
+import cherhy.jung.gptinterview.domain.question.entity.QuestionHistory
+import com.querydsl.core.types.Projections
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 interface QuestionHistoryRepositoryCustom {
-    fun findByCustomerId(customerId: Long, pageable: Pageable)
+    fun findAllByCustomerId(customerId: Long, pageable: Pageable): List<QuestionHistoryResponseR>
+    fun findByCustomerIdAndToken(customerId: Long, token: String): QuestionHistoryResponseR
 }
 
 class QuestionHistoryRepositoryCustomImpl : QuestionHistoryRepositoryCustom,
-    QuerydslRepositorySupport(Question::class.java) {
+    QuerydslRepositorySupport(QuestionHistory::class.java) {
 
-    override fun findByCustomerId(customerId: Long, pageable: Pageable) {
-//        findByCustomerIdOrderByCreatedAtDesc 처럼 동작하게!!
-    }
+    override fun findAllByCustomerId(customerId: Long, pageable: Pageable): List<QuestionHistoryResponseR> =
+        from(questionHistory)
+            .select(
+                Projections.constructor(
+                    QuestionHistoryResponseR::class.java,
+                    questionHistory.token,
+                    question.title,
+                    questionHistory.answer,
+                    questionHistory.feedback,
+                    question.questionType,
+                    question.level,
+                )
+            )
+            .join(question)
+            .on(questionHistory.questionId.eq(question.id))
+            .where(questionHistory.customerId.eq(customerId))
+            .orderBy(questionHistory.createdAt.desc())
+            .limit(pageable.pageSize.toLong())
+            .offset(pageable.offset)
+            .fetch()
+
+    override fun findByCustomerIdAndToken(customerId: Long, token: String): QuestionHistoryResponseR =
+        from(questionHistory)
+            .select(
+                Projections.constructor(
+                    QuestionHistoryResponseR::class.java,
+                    questionHistory.token,
+                    question.title,
+                    questionHistory.answer,
+                    questionHistory.feedback,
+                    question.questionType,
+                    question.level,
+                )
+            )
+            .join(question)
+            .on(questionHistory.questionId.eq(question.id))
+            .where(
+                questionHistory.customerId.eq(customerId),
+                questionHistory.token.eq(token)
+            )
+            .fetchOne()
 
 }
+
