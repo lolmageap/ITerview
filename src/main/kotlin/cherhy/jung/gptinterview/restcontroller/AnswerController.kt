7728ba866539de1da71f50203cc9244382aeb92f
cherhy <ekxk1234@naver.com>
2023-11-24 23:56:10 +0900

    separate AnswerController

---
+++ src/main/kotlin/cherhy/jung/gptinterview/restcontroller/AnswerController.kt
@@ -0,0 +1,44 @@
+package cherhy.jung.gptinterview.restcontroller
+
+import cherhy.jung.gptinterview.domain.customer.AuthCustomer
+import cherhy.jung.gptinterview.domain.question.QuestionHistoryReadService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
+import org.springframework.http.HttpStatus
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.*
+
+@Tag(name = "답변")
+@RestController
+@RequestMapping("/answers")
+class AnswerController(
+    private val questionHistoryReadService: QuestionHistoryReadService,
+) {
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/histories")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "답변 내역", description = "풀었던 질문들을 확인한다.")
+    fun getAnswerHistories(
+        @AuthenticationPrincipal authCustomer: AuthCustomer,
+        @Parameter(hidden = true) @PageableDefault(size = 20, page = 0) pageable: Pageable,
+    ): List<AnswerResponse> =
+        questionHistoryReadService.getAllQuestionHistories(authCustomer.customerId, pageable)
+            .map(AnswerResponse::of)
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/histories/{token}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "답변 내역", description = "풀었던 질문의 답변 내용을 포함하여 조회한다.")
+    fun getAnswerHistory(
+        @AuthenticationPrincipal authCustomer: AuthCustomer,
+        @PathVariable token: String,
+    ): AnswerResponse =
+        questionHistoryReadService.getQuestionHistory(authCustomer.customerId, token)
+            .let(AnswerResponse::of)
+
+}
--- src/main/kotlin/cherhy/jung/gptinterview/restcontroller/GptController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/restcontroller/GptController.kt
@@ -1,24 +1,19 @@
 package cherhy.jung.gptinterview.restcontroller
 
 import cherhy.jung.gptinterview.domain.customer.AuthCustomer
-import cherhy.jung.gptinterview.domain.question.QuestionHistoryReadService
 import cherhy.jung.gptinterview.usecase.GptAnswerUseCase
 import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.data.domain.Pageable
-import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
-@Tag(name = "답변")
+@Tag(name = "피드백")
 @RestController
 @RequestMapping("/answers")
 class GptController(
     private val gptAnswerUseCase: GptAnswerUseCase,
-    private val questionHistoryReadService: QuestionHistoryReadService,
 ) {
 
     @PostMapping
@@ -31,26 +26,5 @@ class GptController(
     ): String =
         gptAnswerUseCase.requestAnswerToGpt(authCustomer.customerId, gptRequest)
 
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/histories")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "답변 내역", description = "풀었던 질문들을 확인한다.")
-    fun getAnswerHistories(
-        @AuthenticationPrincipal authCustomer: AuthCustomer,
-        @Parameter(hidden = true) @PageableDefault(size = 20, page = 0) pageable: Pageable,
-    ): List<AnswerResponse> =
-        questionHistoryReadService.getAllQuestionHistories(authCustomer.customerId, pageable)
-            .map(AnswerResponse::of)
-
-    @PreAuthorize("isAuthenticated()")
-    @GetMapping("/histories/{token}")
-    @ResponseStatus(HttpStatus.OK)
-    @Operation(summary = "답변 내역", description = "풀었던 질문의 답변 내용을 포함하여 조회한다.")
-    fun getAnswerHistory(
-        @AuthenticationPrincipal authCustomer: AuthCustomer,
-        @PathVariable token: String,
-    ): AnswerResponse =
-        questionHistoryReadService.getQuestionHistory(authCustomer.customerId, token)
-            .let(AnswerResponse::of)
 
 }
