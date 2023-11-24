1a0437356925273d3301d116b78fe2129cac14ca
cherhy <ekxk1234@naver.com>
2023-11-25 01:25:29 +0900

    add function : clearCookie

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -2,3 +2,7 @@ const getCookie = () => {
     const authorization = document.cookie.replace(/(?:(?:^|.*;\s*)Authorization\s*=\s*([^;]*).*$)|^.*$/, "$1")
     return authorization
 }
+
+const clearCookie = () => {
+    document.cookie = 'Authorization=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+}
