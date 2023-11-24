611af1d9c9b8eccba523ed656765d2115fa4a92b
cherhy <ekxk1234@naver.com>
2023-11-24 23:57:17 +0900

    import dependency

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/entity/QuestionHistory.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/entity/QuestionHistory.kt
@@ -2,7 +2,9 @@ package cherhy.jung.gptinterview.domain.question.entity
 
 import cherhy.jung.gptinterview.util.BaseEntity
 import cherhy.jung.gptinterview.util.Generator
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.Lob
 
 @Entity
 class QuestionHistory(
