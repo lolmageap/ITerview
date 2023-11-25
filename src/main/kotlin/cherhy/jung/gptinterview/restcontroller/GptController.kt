4632a789f4122d585209b0c033924aaf1c31807a
cherhy <ekxk1234@naver.com>
2023-11-25 23:36:35 +0900

    add get answer key method

--- src/main/kotlin/cherhy/jung/gptinterview/restcontroller/GptController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/restcontroller/GptController.kt
@@ -9,22 +9,31 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
-@Tag(name = "피드백")
+@Tag(name = "GPT 요청")
 @RestController
-@RequestMapping("/answers")
 class GptController(
     private val gptAnswerUseCase: GptAnswerUseCase,
 ) {
 
-    @PostMapping
+    @PostMapping("/answers")
     @PreAuthorize("isAuthenticated()")
     @ResponseStatus(HttpStatus.CREATED)
     @Operation(summary = "답변 하기", description = "질문을 풀고 답안을 제출한 뒤 점수 및 피드백을 받는다.")
-    fun postAndGet(
+    fun requestAnswer(
         @RequestBody gptRequest: GptRequest,
         @AuthenticationPrincipal authCustomer: AuthCustomer,
     ): String =
         gptAnswerUseCase.requestAnswerToGpt(authCustomer.customerId, gptRequest)
+
+    @GetMapping("/answers/{token}")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "답변 하기", description = "질문을 풀고 답안을 제출한 뒤 점수 및 피드백을 받는다.")
+    fun getOnlyFeedback(
+        @PathVariable token: String,
+        @AuthenticationPrincipal authCustomer: AuthCustomer,
+    ): String =
+        gptAnswerUseCase.requestOnlyFeedbackToGpt(authCustomer.customerId, token)
 
 
 }
