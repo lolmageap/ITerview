1ee8e584569ac766b1fc29ae262990211def7718
cherhy <ekxk1234@naver.com>
2023-11-25 23:28:18 +0900

    edit question title

--- src/main/resources/sql/framework/dotnet-question.sql
+++ src/main/resources/sql/framework/dotnet-question.sql
@@ -15,7 +15,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'CTS 란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 CTS 란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -23,7 +23,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'CLR이란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 CLR이란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -31,7 +31,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'CLS 란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 CLS 란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -39,7 +39,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'JIT 란?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 JIT 란?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -47,7 +47,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'MSIL이란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 MSIL이란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -55,7 +55,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '관리 코드와 비 관리 코드는 무엇을 의미합니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 관리 코드와 비 관리 코드는 무엇을 의미합니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -63,7 +63,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '매니지드 코드는 어떻게 실행 되나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
+VALUES (0, NOW(), NOW(), '.Net에서 매니지드 코드는 어떻게 실행 되나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -135,7 +135,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'CAS (코드 액세스 보안)를 설명하십시오.', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
+VALUES (0, NOW(), NOW(), '.Net에서 CAS (코드 액세스 보안)를 설명하십시오.', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -143,7 +143,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'GAC 란?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 GAC 란?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -151,7 +151,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '세계화와 현지화는 무엇을 의미합니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 세계화와 현지화는 무엇을 의미합니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -159,7 +159,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '가비지 콜렉터 란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 가비지 콜렉터 란 무엇입니까?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -167,7 +167,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'using 이라는 키워드는 네임스페이스를 링크하기 위해서 사용하기도 하지만 객체를 생성할 때 또한 사용되기도 합니다. 이때 using을 이용하는 목적에 대해서 설명해주세요.', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 using 이라는 키워드는 네임스페이스를 링크하기 위해서 사용하기도 하지만 객체를 생성할 때 또한 사용되기도 합니다. 이때 using을 이용하는 목적에 대해서 설명해주세요.', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -175,7 +175,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '3-Tier 기반의 프로젝트는 무엇인지 그리고 3-Tier기반의 시스템이 가지는 장점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
+VALUES (0, NOW(), NOW(), '.Net에서 3-Tier 기반의 프로젝트는 무엇인지 그리고 3-Tier기반의 시스템이 가지는 장점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -183,7 +183,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '메모리 단편화를 해결할수있는 기법은 무엇이 있나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
+VALUES (0, NOW(), NOW(), '.Net에서 메모리 단편화를 해결할수있는 기법은 무엇이 있나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -191,7 +191,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '소멸자에 virtual을 쓰는이유는 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 소멸자에 virtual을 쓰는이유는 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -199,7 +199,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '포함과 상속의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 포함과 상속의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -207,7 +207,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, modified_at)
 VALUES (0, @last_id_in_question, 'DOTNET', NOW(), NOW());
 
 INSERT INTO question (id, created_at, modified_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'STL에서 erase와 remove의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), '.Net에서 STL에서 erase와 remove의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
