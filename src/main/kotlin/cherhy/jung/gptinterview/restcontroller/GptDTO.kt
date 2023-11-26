3e1269d7d95ee1bb2ce4b4cae399ea82148b32b5
lolmageap <ekxk1234@naver.com>
2023-11-26 21:52:55 +0900

    create controller layer gpt response dto

--- src/main/kotlin/cherhy/jung/gptinterview/restcontroller/GptDTO.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/restcontroller/GptDTO.kt
@@ -3,6 +3,7 @@ package cherhy.jung.gptinterview.restcontroller
 import cherhy.jung.gptinterview.domain.question.constant.QuestionLevel
 import cherhy.jung.gptinterview.domain.question.constant.QuestionType
 import cherhy.jung.gptinterview.domain.question.dto.QuestionHistoryResponseS
+import cherhy.jung.gptinterview.usecase.GptResponseS
 
 
 data class GptRequest(
@@ -31,3 +32,15 @@ data class AnswerResponse(
     }
 }
 
+data class GptResponse(
+    val token: String,
+    val body: String,
+) {
+    companion object {
+        fun of(responseS: GptResponseS): GptResponse =
+            GptResponse(
+                token = responseS.token,
+                body = responseS.body,
+            )
+    }
+}
