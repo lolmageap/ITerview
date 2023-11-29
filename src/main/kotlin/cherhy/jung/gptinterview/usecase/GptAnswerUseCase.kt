f88d9dbf89706a4e3248e17a7737f412b7234626
cherhy <ekxk1234@naver.com>
2023-11-29 23:27:12 +0900

    refactoring business logic from usecase

--- src/main/kotlin/cherhy/jung/gptinterview/usecase/GptAnswerUseCase.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/usecase/GptAnswerUseCase.kt
@@ -5,6 +5,7 @@ import cherhy.jung.gptinterview.domain.customer.CustomerReadService
 import cherhy.jung.gptinterview.domain.gpt.GptApi
 import cherhy.jung.gptinterview.domain.question.QuestionHistoryWriteService
 import cherhy.jung.gptinterview.domain.question.QuestionReadService
+import cherhy.jung.gptinterview.domain.question.dto.QuestionHistoryRequestS
 import cherhy.jung.gptinterview.restcontroller.GptRequest
 import cherhy.jung.gptinterview.util.Generator
 import cherhy.jung.gptinterview.util.Validator
@@ -22,35 +23,25 @@ class GptAnswerUseCase(
     fun requestAnswerToGpt(customerId: Long, gptRequest: GptRequest): GptResponseS {
         val customer = customerReadService.getCustomerById(customerId)
         val question = questionReadService.getQuestionByToken(gptRequest.questionToken)
-
         val questionToGpt = Generator.generateQuestionToGpt(
             question = question.title,
             answer = gptRequest.answer,
         )
-
         val feedback = gptApi.generateText(questionToGpt)
         val modifiedFeedback = Validator.validateJsonFormat(feedback)
-
-        val history = questionHistoryWriteService.addHistory(
-            question.id, customer.id, modifiedFeedback, gptRequest.answer,
-        )
-
+        val questionHistory = QuestionHistoryRequestS(question.id, customer.id, modifiedFeedback, gptRequest.answer)
+        val history = questionHistoryWriteService.addHistory(questionHistory)
         return GptResponseS(history, modifiedFeedback)
     }
 
     fun requestOnlyAnswerKeyToGpt(customerId: Long, token: String): GptResponseS {
         val customer = customerReadService.getCustomerById(customerId)
         val question = questionReadService.getQuestionByToken(token)
-
         val questionToGpt = Generator.generateAnswerKeyToGpt(question.title)
-
         val feedback = gptApi.generateText(questionToGpt)
         val modifiedFeedback = Validator.validateJsonFormat(feedback)
-
-        val history = questionHistoryWriteService.addHistory(
-            question.id, customer.id, modifiedFeedback,
-        )
-
+        val questionHistory = QuestionHistoryRequestS(question.id, customer.id, modifiedFeedback)
+        val history = questionHistoryWriteService.addHistory(questionHistory)
         return GptResponseS(history, modifiedFeedback)
     }
 
