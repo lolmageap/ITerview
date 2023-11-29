65147d61f95576c9cfb46a5fa543b6e754a41670
cherhy <ekxk1234@naver.com>
2023-11-29 23:26:15 +0900

    create history request service dto

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryWriteService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryWriteService.kt
@@ -1,6 +1,7 @@
 package cherhy.jung.gptinterview.domain.question
 
 import cherhy.jung.gptinterview.annotation.WriteService
+import cherhy.jung.gptinterview.domain.question.dto.QuestionHistoryRequestS
 import cherhy.jung.gptinterview.domain.question.entity.QuestionHistory
 
 @WriteService
@@ -8,17 +9,14 @@ class QuestionHistoryWriteService(
     private val questionHistoryWriteService: QuestionHistoryRepository,
 ) {
     fun addHistory(
-        questionId: Long,
-        customerId: Long,
-        feedback: String,
-        answer: String = "정답을 가르쳐줘",
+        questionHistoryRequestS: QuestionHistoryRequestS,
     ): String =
         questionHistoryWriteService.save(
             QuestionHistory(
-                questionId = questionId,
-                customerId = customerId,
-                answer = answer,
-                feedback = feedback,
+                questionId = questionHistoryRequestS.questionId,
+                customerId = questionHistoryRequestS.customerId,
+                answer = questionHistoryRequestS.feedback,
+                feedback = questionHistoryRequestS.answer,
             )
         ).token
 }
