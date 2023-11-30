d601c7ec47ebc8a2010fb9fb10c9c4a86708c483
cherhy <ekxk1234@naver.com>
2023-11-30 20:54:05 +0900

    fix save history

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryWriteService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryWriteService.kt
@@ -1,23 +1,17 @@
 package cherhy.jung.gptinterview.domain.question
 
 import cherhy.jung.gptinterview.annotation.WriteService
-import cherhy.jung.gptinterview.domain.question.dto.QuestionHistoryRequestS
+import cherhy.jung.gptinterview.domain.question.dto.QuestionHistoryResponseS
 import cherhy.jung.gptinterview.domain.question.entity.QuestionHistory
 
 @WriteService
 class QuestionHistoryWriteService(
     private val questionHistoryWriteService: QuestionHistoryRepository,
 ) {
-    fun addHistory(
-        questionHistoryRequestS: QuestionHistoryRequestS,
-    ): String =
-        questionHistoryWriteService.save(
-            QuestionHistory(
-                questionId = questionHistoryRequestS.questionId,
-                customerId = questionHistoryRequestS.customerId,
-                answer = questionHistoryRequestS.feedback,
-                feedback = questionHistoryRequestS.answer,
-            )
-        ).token
+
+    fun addHistory(questionHistory: QuestionHistory): QuestionHistoryResponseS =
+        questionHistoryWriteService.save(questionHistory)
+            .let(QuestionHistoryResponseS::of)
+
 }
 
