b0b6a954d39e1013fa3512041b1d4c1af44b183a
cherhy <ekxk1234@naver.com>
2023-12-01 23:53:00 +0900

    create send mail use case

---
+++ src/main/kotlin/cherhy/jung/gptinterview/usecase/SendMailUseCase.kt
@@ -0,0 +1,23 @@
+package cherhy.jung.gptinterview.usecase
+
+import cherhy.jung.gptinterview.annotation.UseCase
+import cherhy.jung.gptinterview.domain.customer.CustomerReadService
+import cherhy.jung.gptinterview.mail.MailComponent
+import cherhy.jung.gptinterview.redis.RedisWriteService
+import cherhy.jung.gptinterview.util.Generator
+
+@UseCase
+class SendMailUseCase(
+    private val customerReadService: CustomerReadService,
+    private val mailComponent: MailComponent,
+    private val redisWriteService: RedisWriteService,
+) {
+
+    fun sendCertificate(email: String) {
+        customerReadService.checkDuplicatedEmail(email)
+        val certificate = Generator.createCertificate()
+        mailComponent.sendMessage(email, certificate)
+        redisWriteService.addCertificate(email, certificate)
+    }
+
+}
