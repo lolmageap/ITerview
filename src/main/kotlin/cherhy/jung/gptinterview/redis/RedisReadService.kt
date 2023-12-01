961b02337d3fd33d2124f6bb3812e3dea7398ca6
cherhy <ekxk1234@naver.com>
2023-12-01 23:52:28 +0900

    add getCertificate method

--- src/main/kotlin/cherhy/jung/gptinterview/redis/RedisReadService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/redis/RedisReadService.kt
@@ -1,7 +1,10 @@
 package cherhy.jung.gptinterview.redis
 
 import cherhy.jung.gptinterview.annotation.ReadService
+import cherhy.jung.gptinterview.exception.DomainName.CERTIFICATE_NUMBER
+import cherhy.jung.gptinterview.exception.NotFoundException
 import cherhy.jung.gptinterview.redis.RedisKey.ACCESS_TOKEN
+import cherhy.jung.gptinterview.redis.RedisKey.CERTIFICATE
 import cherhy.jung.gptinterview.redis.RedisKey.QUESTION_TOKEN
 import org.springframework.data.redis.core.RedisTemplate
 
@@ -9,14 +12,21 @@ import org.springframework.data.redis.core.RedisTemplate
 class RedisReadService(
     private val redisTemplate: RedisTemplate<String, Any>,
 ) {
-    fun getJwtToken(accessToken: String): String? {
-        return redisTemplate.opsForValue().get(ACCESS_TOKEN + accessToken) as? String
-    }
+    fun getJwtToken(accessToken: String): String? =
+        redisTemplate.opsForValue()
+            .get(ACCESS_TOKEN + accessToken)
+            ?.toString()
+
 
     fun getQuestionTokens(customerId: Long, start: Long = 0, end: Long = -1): MutableList<String> =
         redisTemplate.opsForList()
             .range(QUESTION_TOKEN + customerId, start, end)
             ?.map { it.toString() }?.toMutableList()
             ?: mutableListOf()
+
+    fun getCertificate(certificate: String) {
+        redisTemplate.opsForValue().get(CERTIFICATE + certificate)
+            ?: throw NotFoundException(CERTIFICATE_NUMBER)
+    }
 
 }
