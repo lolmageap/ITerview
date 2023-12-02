c2a1a60a8e24d9e6255cdd8237115050f21d25a9
lolmageap <ekxk1234@naver.com>
2023-12-02 22:38:47 +0900

    add edit password method

--- src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerWriteService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerWriteService.kt
@@ -1,13 +1,20 @@
 package cherhy.jung.gptinterview.domain.customer
 
 import cherhy.jung.gptinterview.annotation.WriteService
+import org.springframework.data.repository.findByIdOrNull
 
 @WriteService
 class CustomerWriteService(
     private val customerRepository: CustomerRepository,
 ) {
     fun saveCustomer(customer: Customer, password: String) {
-        customer.changePassword(password)
+        customer.editPassword(password)
+        customerRepository.save(customer)
+    }
+
+    fun editPassword(id: Long, newPassword: String) {
+        val customer = customerRepository.findByIdOrNull(id)!!
+        customer.editPassword(newPassword)
         customerRepository.save(customer)
     }
 
