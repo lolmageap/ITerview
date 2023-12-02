7d7bde733341df7bf1cd152b23ed79778ca232e3
cherhy <ekxk1234@naver.com>
2023-12-03 01:59:49 +0900

    create customer write service test

---
+++ src/test/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerWriteServiceTest.kt
@@ -0,0 +1,40 @@
+package cherhy.jung.gptinterview.domain.customer
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.comparables.shouldBeGreaterThan
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+
+@SpringBootTest
+class CustomerWriteServiceTest(
+    @Autowired private val customerWriteService: CustomerWriteService,
+    @Autowired private val customerRepository: CustomerRepository,
+) : BehaviorSpec({
+
+    Given("회원이 존재하고 ") {
+        val customer = customerRepository.save(
+            Customer(
+                name = "정철희",
+                email = "ekxk1234@naver.com",
+                password = "abcd1234",
+                salt = "random",
+                token = "random",
+            )
+        )
+        val newPassword = "newPassword"
+
+        When("비밀번호를 수정하면 ") {
+            customerWriteService.editPassword(customer.id, newPassword)
+            then("비밀번호가 변경된다.") {
+                val findCustomer = customerRepository.findByIdOrNull(customer.id)!!
+                findCustomer.password shouldNotBe customer.password
+                findCustomer.password shouldBe newPassword
+                findCustomer.modifiedAt!! shouldBeGreaterThan findCustomer.createdAt!!
+            }
+        }
+    }
+
+})
