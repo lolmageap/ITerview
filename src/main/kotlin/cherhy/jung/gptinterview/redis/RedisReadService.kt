2ef08a9d150e3d2c9a04cc47cfa0ec8c33d78edb
lolmageap <ekxk1234@naver.com>
2023-12-02 22:44:59 +0900

    refactoring getJwtToken and checkCertificate

--- src/main/kotlin/cherhy/jung/gptinterview/redis/RedisReadService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/redis/RedisReadService.kt
@@ -2,20 +2,23 @@ package cherhy.jung.gptinterview.redis
 
 import cherhy.jung.gptinterview.annotation.ReadService
 import cherhy.jung.gptinterview.exception.DomainName.CERTIFICATE_NUMBER
+import cherhy.jung.gptinterview.exception.DomainName.EMAIL
 import cherhy.jung.gptinterview.exception.NotFoundException
 import cherhy.jung.gptinterview.redis.RedisKey.ACCESS_TOKEN
 import cherhy.jung.gptinterview.redis.RedisKey.CERTIFICATE
 import cherhy.jung.gptinterview.redis.RedisKey.QUESTION_TOKEN
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.security.access.AccessDeniedException
 
 @ReadService
 class RedisReadService(
     private val redisTemplate: RedisTemplate<String, Any>,
 ) {
-    fun getJwtToken(accessToken: String): String? =
+    fun getJwtToken(accessToken: String): String =
         redisTemplate.opsForValue()
             .get(ACCESS_TOKEN + accessToken)
             ?.toString()
+            ?: throw AccessDeniedException("잘못된 토큰")
 
 
     fun getQuestionTokens(customerId: Long, start: Long = 0, end: Long = -1): MutableList<String> =
@@ -24,9 +27,12 @@ class RedisReadService(
             ?.map { it.toString() }?.toMutableList()
             ?: mutableListOf()
 
-    fun getCertificate(certificate: String) {
-        redisTemplate.opsForValue().get(CERTIFICATE + certificate)
-            ?: throw NotFoundException(CERTIFICATE_NUMBER)
+    fun checkCertificate(email: String, certificateNumber: String) {
+        val certificate = redisTemplate.opsForValue().getAndDelete(CERTIFICATE + email)
+            ?.toString()
+            ?: throw NotFoundException(EMAIL)
+
+        if (certificateNumber != certificate) throw NotFoundException(CERTIFICATE_NUMBER)
     }
 
 }
