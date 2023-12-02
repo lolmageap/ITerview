70dd7671a5d2c778c53ab25daa9228881371872f
lolmageap <ekxk1234@naver.com>
2023-12-02 22:43:35 +0900

    add static import

--- src/main/kotlin/cherhy/jung/gptinterview/restcontroller/PositionController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/restcontroller/PositionController.kt
@@ -4,12 +4,8 @@ import cherhy.jung.gptinterview.domain.position.PositionFactory
 import cherhy.jung.gptinterview.domain.position.PositionType
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpStatus.OK
+import org.springframework.web.bind.annotation.*
 
 @Tag(name = "포지션")
 @RestController
@@ -17,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController
 class PositionController {
 
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(OK)
     @Operation(summary = "포지션 정보", description = "요청한 포지션의 상세 스펙에 대해 조회한다.")
     fun getPosition(
         @RequestParam positionTypes: List<PositionType>,
--- src/main/kotlin/cherhy/jung/gptinterview/restcontroller/QuestionController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/restcontroller/QuestionController.kt
@@ -1,9 +1,9 @@
 package cherhy.jung.gptinterview.restcontroller
 
 import cherhy.jung.gptinterview.domain.customer.AuthCustomer
+import cherhy.jung.gptinterview.domain.question.QuestionReadService
 import cherhy.jung.gptinterview.redis.RedisReadService
 import cherhy.jung.gptinterview.redis.RedisWriteService
-import cherhy.jung.gptinterview.domain.question.QuestionReadService
 import cherhy.jung.gptinterview.util.getEnd
 import cherhy.jung.gptinterview.util.getStart
 import io.swagger.v3.oas.annotations.Operation
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.OK
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
@@ -27,7 +27,7 @@ class QuestionController(
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(OK)
     @Operation(summary = "질문 받기", description = "요청에 맞게 질문을 반환한다.")
     fun getRandomQuestion(
         @AuthenticationPrincipal authCustomer: AuthCustomer,
@@ -44,7 +44,7 @@ class QuestionController(
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/histories")
-    @ResponseStatus(HttpStatus.OK)
+    @ResponseStatus(OK)
     @Operation(summary = "질문 내역", description = "조회했던 질문들을 확인한다.")
     fun getQuestionHistory(
         @AuthenticationPrincipal authCustomer: AuthCustomer,
