1963271bfe9bd21305e43b3d1b6502bc033cb154
lolmageap <ekxk1234@naver.com>
2023-12-02 22:35:04 +0900

    add certificate and edit password

--- src/main/kotlin/cherhy/jung/gptinterview/restcontroller/AuthorityController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/restcontroller/AuthorityController.kt
@@ -1,8 +1,10 @@
 package cherhy.jung.gptinterview.restcontroller
 
-import cherhy.jung.gptinterview.jwt.*
+import cherhy.jung.gptinterview.domain.customer.AuthCustomer
 import cherhy.jung.gptinterview.jwt.JwtFilter.Companion.AUTHORIZATION_HEADER
+import cherhy.jung.gptinterview.jwt.TokenResponse
 import cherhy.jung.gptinterview.redis.RedisReadService
+import cherhy.jung.gptinterview.usecase.EditPasswordUseCase
 import cherhy.jung.gptinterview.usecase.SendMailUseCase
 import cherhy.jung.gptinterview.usecase.SignInUseCase
 import cherhy.jung.gptinterview.usecase.SignUpUseCase
@@ -10,7 +12,10 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.HttpStatus.OK
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "회원")
@@ -20,11 +25,12 @@ class AuthorityController(
     private val signInUseCase: SignInUseCase,
     private val signUpUseCase: SignUpUseCase,
     private val sendMailUseCase: SendMailUseCase,
+    private val editPasswordUseCase: EditPasswordUseCase,
     private val redisReadService: RedisReadService,
 ) {
 
     @PostMapping("/sign-in")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(CREATED)
     @Operation(summary = "로그인", description = "로그인을 하고 토큰을 발급 받는다.")
     fun signIn(
         @Valid @RequestBody signInRequest: SignInRequest,
@@ -35,7 +41,7 @@ class AuthorityController(
         }
 
     @PostMapping("/sign-up")
-    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseStatus(CREATED)
     @Operation(summary = "회원가입", description = "회원가입을 하고 로그인 상태가 되며 토큰을 발급 받는다.")
     fun signUp(
         @Valid @RequestBody signUpRequest: SignUpRequest,
@@ -47,14 +53,40 @@ class AuthorityController(
         }
     }
 
+    // TODO : 이메일 양식 검사 로직 추가
+
     @PostMapping("/certificates")
+    @ResponseStatus(CREATED)
+    @Operation(summary = "이메일로 인증번호 전송", description = "이메일로 인증번호를 보내고 3분간 인증번호를 저장한다.")
     fun sendCertificate(@RequestBody email: String) {
         sendMailUseCase.sendCertificate(email)
     }
 
     @GetMapping("/certificates")
-    fun getCertificate(@RequestParam certificate: String) {
-        redisReadService.getCertificate(certificate)
+    @ResponseStatus(OK)
+    @Operation(summary = "인증번호를 검증한다.", description = "이메일로 발급받은 인증번호를 3분안에 검증한다.")
+    fun getCertificate(@RequestParam certificate: String, @RequestParam email: String) {
+        redisReadService.checkCertificate(email, certificate)
     }
+
+
+    @PatchMapping("/reset-password")
+    @ResponseStatus(OK)
+    @Operation(summary = "비밀번호 초기화", description = "비밀번호를 초기화하고 초기화한 비밀번호를 이메일로 보내준다.")
+    fun resetPassword(@RequestParam certificate: String, @RequestParam email: String) {
+        redisReadService.checkCertificate(email, certificate)
+        val resetPassword = editPasswordUseCase.resetPassword(email)
+        sendMailUseCase.sendResetPassword(email, resetPassword)
+    }
+
+    @PatchMapping("/password")
+    @ResponseStatus(OK)
+    @PreAuthorize("isAuthenticated()")
+    @Operation(summary = "비밀번호 수정", description = "비밀번호를 수정하고 수정된 비밀번호를 이메일로 보내준다.")
+    fun editPassword(
+        @RequestBody editPasswordRequest: EditPasswordRequest,
+        @AuthenticationPrincipal authCustomer: AuthCustomer,
+    ) =
+        editPasswordUseCase.editPassword(authCustomer.customerId, editPasswordRequest.toEditPasswordRequestS())
 
 }
