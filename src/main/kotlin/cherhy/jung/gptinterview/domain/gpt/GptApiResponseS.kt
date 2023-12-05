e33fe305fbfb18bbd7b8b8b66de79fa99f4aaff5
cherhy <ekxk1234@naver.com>
2023-12-05 23:30:21 +0900

    remove json property

--- src/main/kotlin/cherhy/jung/gptinterview/domain/gpt/GptApiResponseS.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/gpt/GptApiResponseS.kt
@@ -5,18 +5,18 @@ import com.fasterxml.jackson.annotation.JsonProperty
 // S의 postfix는 무슨뜻일까요?~
 // jsonProperty가 필요하지 않은애들은 뺼까요?
 data class GptApiResponseS(
-    @JsonProperty("id") val id: String,
+    val id: String,
     @JsonProperty("object") val obj: String,
-    @JsonProperty("created") val created: Long,
-    @JsonProperty("model") val model: String,
-    @JsonProperty("choices") val choices: List<Choice>,
-    @JsonProperty("usage") val usage: Usage,
+    val created: Long,
+    val model: String,
+    val choices: List<Choice>,
+    val usage: Usage,
 )
 
 data class Choice(
-    @JsonProperty("text") val text: String,
-    @JsonProperty("index") val index: Int,
-    @JsonProperty("logprobs") val logprobs: Any?,
+    val text: String,
+    val index: Int,
+    val logprobs: Any?,
     @JsonProperty("finish_reason") val finishReason: String,
 )
 
