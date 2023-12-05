d5125c51797dd0fb5de6b4b1472c2e47c543f10a
cherhy <ekxk1234@naver.com>
2023-12-05 23:27:55 +0900

    line break

--- src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerRepository.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerRepository.kt
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface CustomerRepository : JpaRepository<Customer, Long> {
     // 메서드와 메서드 사이는 고백을 넣어볼까요?
     fun existsByEmail(email: String): Boolean
+
     fun findByEmail(email: String): Customer?
 
 }
