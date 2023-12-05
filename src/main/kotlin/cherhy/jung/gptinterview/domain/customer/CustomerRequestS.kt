bb95b2ce7a0ca555a7550aba6ff5754030b4905e
cherhy <ekxk1234@naver.com>
2023-12-05 23:32:03 +0900

    fix companion object function to local function

--- src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerRequestS.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerRequestS.kt
@@ -8,15 +8,16 @@ data class CustomerRequestS(
     var password: String,
 ) {
     val salt: String = Generator.generateSalt()
+
+    fun toCustomer(): Customer = Customer(
+        name = name,
+        email = email,
+        password = password,
+        token = Generator.generateToken(),
+        salt = salt,
+    )
 }
 
 // 확장함수로 해야할까요?~
 // 직접 만든 클래스는 확장함수를 만들지 않아도 될것 같아요.
 // 자바나 스프링에서 제공하는 클래스(내가 수정 못하는) 그런 애들은 만들만 하겠네요!
-fun CustomerRequestS.toCustomer(): Customer = Customer(
-    name = name,
-    email = email,
-    password = password,
-    token = Generator.generateToken(),
-    salt = salt,
-)
