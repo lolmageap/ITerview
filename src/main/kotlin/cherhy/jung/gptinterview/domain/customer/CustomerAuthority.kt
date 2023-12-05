2a49b545978df23cf20f8aa437c8337519f28972
cherhy <ekxk1234@naver.com>
2023-12-05 23:27:41 +0900

    add column size

--- src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerAuthority.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerAuthority.kt
@@ -10,6 +10,7 @@ class CustomerAuthority(
     var id: Long = 0,
 
     // 기본적으로 Enum은 @Column을 정의해서 길이를 조금 넣어볼까요?
+    @Column(length = 10)
     @Enumerated(EnumType.STRING)
     val role: CustomerRole,
 )
