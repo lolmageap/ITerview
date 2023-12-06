e8fab8491cf1920ea8e5a8769e9dc06f9cb657da
cherhy <ekxk1234@naver.com>
2023-12-06 23:43:26 +0900

    remove pageable index

--- src/main/kotlin/cherhy/jung/gptinterview/config/JpaConfig.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/config/JpaConfig.kt
@@ -1,20 +1,10 @@
 package cherhy.jung.gptinterview.config
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer
 
 @Configuration
 @EnableJpaAuditing
 class JpaConfig {
-
-    // *require review: jpa에서는 0이 1페이지라서, 이 부분을 쓰신거 같은데 이거는 application.properties(yml)에서 설정이 가능하지 않나요?
-    @Bean
-    fun customize(): PageableHandlerMethodArgumentResolverCustomizer? {
-        return PageableHandlerMethodArgumentResolverCustomizer {
-            it.setOneIndexedParameters(true)
-        }
-    }
 
 }
