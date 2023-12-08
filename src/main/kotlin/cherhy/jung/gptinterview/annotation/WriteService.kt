1f5f81b67bb0bdeff20915a50a684b25481e0c4d
lolmageap <ekxk1234@naver.com>
2023-12-09 01:21:37 +0900

    refactoring annotation

--- src/main/kotlin/cherhy/jung/gptinterview/annotation/WriteService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/annotation/WriteService.kt
@@ -1,14 +1,8 @@
 package cherhy.jung.gptinterview.annotation
 
-import org.springframework.core.annotation.AliasFor
-import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-@MustBeDocumented
-@Component
+@Service
 @Transactional(readOnly = false)
-annotation class WriteService(
-    @get:AliasFor(annotation = Component::class) val value: String = "",
-)
+annotation class WriteService
