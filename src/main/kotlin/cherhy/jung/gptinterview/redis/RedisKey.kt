ae32b517ea40834ec0b2b410f76ffc0dc85e2c08
cherhy <ekxk1234@naver.com>
2023-12-14 23:38:16 +0900

    access token rename to refresh token

--- src/main/kotlin/cherhy/jung/gptinterview/redis/RedisKey.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/redis/RedisKey.kt
@@ -1,7 +1,7 @@
 package cherhy.jung.gptinterview.redis
 
 object RedisKey {
-    const val ACCESS_TOKEN: String = "accessToken:"
+    const val REFRESH_TOKEN: String = "refreshToken:"
     const val QUESTION_TOKEN: String = "questionToken:"
     const val CERTIFICATE: String = "certificate:"
 }
