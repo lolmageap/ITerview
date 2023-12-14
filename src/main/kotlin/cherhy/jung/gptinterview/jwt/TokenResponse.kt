77241f5f35e38463e439747b5779a36870d52d8b
cherhy <ekxk1234@naver.com>
2023-12-14 23:33:31 +0900

    add jwt response dto

--- src/main/kotlin/cherhy/jung/gptinterview/jwt/TokenResponse.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/jwt/TokenResponse.kt
@@ -1,7 +1,30 @@
 package cherhy.jung.gptinterview.jwt
 
+import java.util.*
+
+data class AccessTokenResponse(
+    val accessToken: String,
+    val accessTokenExpireTime: Date,
+) {
+    companion object {
+        fun of(tokenResponseS: TokenResponseS) =
+            AccessTokenResponse(
+                accessToken = tokenResponseS.token,
+                accessTokenExpireTime = tokenResponseS.tokenExpireTime,
+            )
+    }
+}
+
 data class TokenResponse(
-    val token: String,
-    val role: List<String>,
+    val accessToken: String,
+    val accessTokenExpireTime: Date,
+    val refreshToken: String,
+    val refreshTokenExpireTime: Date,
+    val roles: List<String>,
     val customerToken: String,
+)
+
+data class TokenResponseS(
+    val token: String,
+    val tokenExpireTime: Date,
 )
