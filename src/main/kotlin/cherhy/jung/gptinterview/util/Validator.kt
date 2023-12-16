b187af2a785cd5ddc56dd8d3fbce969ce8492e66
cherhy <ekxk1234@naver.com>
2023-12-17 00:30:01 +0900

    add validate with scenario

--- src/main/kotlin/cherhy/jung/gptinterview/util/Validator.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/util/Validator.kt
@@ -1,16 +1,26 @@
 package cherhy.jung.gptinterview.util
 
+import cherhy.jung.gptinterview.exception.GptNotGeneratedException
+
 object Validator {
 
-    fun validateJsonFormat(feedback: String): String =
-        if (feedback.trim().endsWith("}")) {
-            feedback
-        } else if (feedback.trim().endsWith("\"\n")) {
-            feedback.plus("}")
-        } else if (feedback.trim().endsWith("\"")) {
-            feedback.plus("}")
-        } else {
-            feedback.plus("\"}")
-        }
+    fun validateJsonFormat(feedback: String): String {
+        val json = feedback.trim()
 
+        if ( json.startsWith("{").not() ||
+            json.contains("score").not() ) {
+            throw GptNotGeneratedException()
+        }
+        
+        return if (json.endsWith("}")) {
+            json
+        } else if (json.endsWith("\"\n")) {
+            json.plus("}")
+        } else if (json.endsWith("\"")) {
+            json.plus("}")
+        } else {
+            json.plus("\"}")
+        }
+    }
+        
 }
