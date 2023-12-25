a7b3dc0f5ac31b1bd6eebd510a80a7d7a2eee882
cherhy <ekxk1234@naver.com>
2023-12-25 20:55:41 +0900

    fix argument to property

--- src/main/kotlin/cherhy/jung/gptinterview/domain/BaseEntity.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/BaseEntity.kt
@@ -1,10 +1,6 @@
 package cherhy.jung.gptinterview.domain
 
-import jakarta.persistence.EntityListeners
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.MappedSuperclass
+import jakarta.persistence.*
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
@@ -12,11 +8,11 @@ import java.time.LocalDateTime
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener::class)
-abstract class BaseEntity(
+abstract class BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    val id: Long = 0,
-) {
+    val id: Long = 0
 
     @CreatedDate
     lateinit var createdAt: LocalDateTime
