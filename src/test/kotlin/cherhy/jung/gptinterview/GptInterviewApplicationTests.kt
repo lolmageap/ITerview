2a91bf3dde774dadb1b215c32537340108a817ac
lolmageap <ekxk1234@naver.com>
2024-03-02 22:55:34 +0900

    encode yml properties

--- src/test/kotlin/cherhy/jung/gptinterview/GptInterviewApplicationTests.kt
+++ src/test/kotlin/cherhy/jung/gptinterview/GptInterviewApplicationTests.kt
@@ -1,12 +1,44 @@
 package cherhy.jung.gptinterview
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import io.kotest.core.spec.style.StringSpec
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor
 
-@SpringBootTest
-internal class GptInterviewApplicationTests {
+internal class GptInterviewApplicationTests : StringSpec({
 
-    @Test
-    fun contextLoads() {
+    val listOfMap = listOf(
+        mapOf("DATABASE_URL" to "jdbc:mysql://localhost:3501/iterview"),
+        mapOf("DATABASE_USERNAME" to "root"),
+        mapOf("DATABASE_PASSWORD" to "wjd0322189!"),
+        mapOf("REDIS_HOST" to "localhost"),
+        mapOf("REDIS_PORT" to "6379"),
+        mapOf("MAIL_HOST" to "smtp.gmail.com"),
+        mapOf("MAIL_PORT" to "587"),
+        mapOf("MAIL_USERNAME" to "ekxk1234@naver.com"),
+        mapOf("MAIL_PASSWORD" to "password"),
+        mapOf("GPT_API_KEY" to "api-key"),
+        mapOf("JWT_SECRET" to "secret"),
+    )
+
+    "encrypt and decrypt text with jasypt" {
+        val encryptKey = System.getenv("JASYPT_PASSWORD")
+        val encryptAlgorithm = System.getenv("JASYPT_ALGORITHM")
+
+        val jasypt = StandardPBEStringEncryptor().apply {
+            setPassword(encryptKey)
+            setAlgorithm(encryptAlgorithm)
+        }
+
+        listOfMap.forEach {
+            it.forEach { (key, value) ->
+                val encrypt = jasypt.encrypt(value)
+                println("$key=$encrypt")
+            }
+        }
+
+        val jwtExpiration = 86400
+        val jwtRefreshExpiration = 86400 * 30
+
+        println("JWT_ACCESS_EXPIRATION=${jasypt.encrypt(jwtExpiration.toString())}")
+        println("JWT_REFRESH_EXPIRATION=${jasypt.encrypt(jwtRefreshExpiration.toString())}")
     }
-}
+})
