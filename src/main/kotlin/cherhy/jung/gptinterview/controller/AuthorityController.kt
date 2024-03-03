6c3566558cfb56e70ee4ad3493b0e95e6acaa2ff
lolmageap <ekxk1234@naver.com>
2024-03-03 18:45:54 +0900

    rename to EditPasswordRequestVo.kt

--- src/main/kotlin/cherhy/jung/gptinterview/controller/AuthorityController.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/controller/AuthorityController.kt
@@ -2,18 +2,19 @@ package cherhy.jung.gptinterview.controller
 
 import cherhy.jung.gptinterview.controller.dto.*
 import cherhy.jung.gptinterview.domain.authority.AuthCustomer
+import cherhy.jung.gptinterview.extension.addAccessTokenInHeader
+import cherhy.jung.gptinterview.extension.addRefreshTokenInHeader
 import cherhy.jung.gptinterview.external.jwt.AccessTokenResponse
 import cherhy.jung.gptinterview.external.jwt.TokenResponse
 import cherhy.jung.gptinterview.external.redis.RedisReadService
 import cherhy.jung.gptinterview.usecase.*
-import cherhy.jung.gptinterview.extension.addAccessTokenInHeader
-import cherhy.jung.gptinterview.extension.addRefreshTokenInHeader
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import jakarta.validation.Valid
-import org.springframework.http.HttpStatus.*
+import org.springframework.http.HttpStatus.CREATED
+import org.springframework.http.HttpStatus.NO_CONTENT
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
@@ -28,7 +29,6 @@ class AuthorityController(
     private val redisReadService: RedisReadService,
     private val regenerateAccessTokenUseCase: RegenerateAccessTokenUseCase,
 ) {
-
     @PostMapping("/sign-in")
     @ResponseStatus(CREATED)
     @Operation(summary = "로그인", description = "로그인을 하고 토큰을 발급 받는다.")
@@ -43,7 +43,7 @@ class AuthorityController(
 
     @PostMapping("/sign-up")
     @ResponseStatus(CREATED)
-    @Operation(summary = "회원가입", description = "회원가입을 하고 로그인 상태가 되며 토큰을 발급 받는다.")
+    @Operation(summary = "회원가입", description = "회원가입 을 하고 로그인 상태가 되며 토큰을 발급 받는다.")
     fun signUp(
         @Valid @RequestBody signUpRequest: SignUpRequest,
         httpServletResponse: HttpServletResponse,
@@ -56,7 +56,7 @@ class AuthorityController(
     }
 
     @PostMapping("/access-tokens")
-    @Operation(summary = "access token 재발급", description = "refresh token 을 넘기면 access token 을 반환한다.")
+    @Operation(summary = "access token 재발급", description = "refresh token 을 넘기면 access token 을 반환 한다.")
     fun getAccessToken(
         httpServletRequest: HttpServletRequest,
         httpServletResponse: HttpServletResponse,
@@ -66,13 +66,13 @@ class AuthorityController(
 
     @PostMapping("/certificates")
     @ResponseStatus(CREATED)
-    @Operation(summary = "이메일로 인증번호 전송", description = "이메일로 인증번호를 보내고 3분간 인증번호를 저장한다.")
+    @Operation(summary = "이메일 로 인증 번호 전송", description = "이메일 로 인증 번호를 보내고 3분간 인증 번호를 저장 한다.")
     fun sendCertificate(@RequestBody @Valid emailRequest: EmailRequest) =
         sendMailUseCase.sendCertificate(emailRequest.email)
 
     @GetMapping("/certificates")
     @ResponseStatus(NO_CONTENT)
-    @Operation(summary = "인증번호를 검증한다.", description = "이메일로 발급받은 인증번호를 3분안에 검증한다.")
+    @Operation(summary = "인증 번호를 검증 한다.", description = "이메일 로 발급 받은 인증번호 를 3분안에 검증 한다.")
     fun getCertificate(
         @RequestParam certificate: String,
         @RequestParam @Valid emailRequest: EmailRequest,
@@ -82,22 +82,21 @@ class AuthorityController(
 
     @PatchMapping("/passwords")
     @ResponseStatus(NO_CONTENT)
-    @Operation(summary = "비밀번호 수정", description = "비밀번호를 수정하고 수정된 비밀번호를 이메일로 보내준다.")
+    @Operation(summary = "비밀번호 수정", description = "비밀번호 를 수정 하고 수정된 비밀번호 를 이메일 로 보내 준다.")
     fun editPassword(
         @RequestBody editPasswordRequest: EditPasswordRequest,
         @AuthenticationPrincipal authCustomer: AuthCustomer,
     ) =
         editPasswordUseCase.editPassword(
             authCustomer.customerId,
-            editPasswordRequest.toEditPasswordRequestS()
+            editPasswordRequest.toEditPasswordRequestVo()
         )
 
     @DeleteMapping("/passwords")
     @ResponseStatus(NO_CONTENT)
-    @Operation(summary = "비밀번호 초기화", description = "비밀번호를 초기화하고 초기화한 비밀번호를 이메일로 보내준다.")
+    @Operation(summary = "비밀번호 초기화", description = "비밀번호 를 초기화 하고 초기화 한 비밀번호 를 이메일 로 보내 준다.")
     fun resetPassword(@RequestBody @Valid certificateRequest: CertificateRequest) {
         redisReadService.checkCertificate(certificateRequest.email, certificateRequest.certificate)
         editPasswordUseCase.resetAndSendPassword(certificateRequest.email)
     }
-
 }
