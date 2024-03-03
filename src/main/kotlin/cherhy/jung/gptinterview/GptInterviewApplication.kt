f23211a447500eda0b90815d062e800a274cdb23
lolmageap <ekxk1234@naver.com>
2024-03-03 18:47:38 +0900

    disable flyway autoconfiguration

--- src/main/kotlin/cherhy/jung/gptinterview/GptInterviewApplication.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/GptInterviewApplication.kt
@@ -1,8 +1,19 @@
 package cherhy.jung.gptinterview
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.FilterType
 
+@ComponentScan(
+    excludeFilters = [
+        ComponentScan.Filter(
+            type = FilterType.CUSTOM,
+            classes = [FlywayAutoConfiguration::class],
+        )
+    ]
+)
 @SpringBootApplication
 class GptInterviewApplication
 
