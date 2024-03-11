9289b44b45c1b0a953b6568a82989f25729ac742
lolmageap <ekxk1234@naver.com>
2024-03-11 23:36:05 +0900

    fix to dbms user version

--- src/main/resources/db/migration/V1.0.1_INIT.sql
+++ src/main/resources/db/migration/V1.0.1_INIT.sql
@@ -1,7 +1,7 @@
-create user 'root'@'host.docker.internal';
+create user 'root'@'172.20.0.1';
 
-ALTER USER 'root'@'host.docker.internal' IDENTIFIED WITH mysql_native_password BY 'your-password';
+ALTER USER 'root'@'172.20.0.1' IDENTIFIED WITH mysql_native_password BY 'wjd0322189!';
 
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'host.docker.internal';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.20.0.1';
 
 FLUSH PRIVILEGES;
