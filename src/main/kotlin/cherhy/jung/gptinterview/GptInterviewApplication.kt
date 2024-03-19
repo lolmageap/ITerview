ef99cf20bbfc72c0066e2f6a86ccf16b0c058a9d
lolmageap <ekxk1234@naver.com>
2024-03-19 22:32:21 +0900

    autoconfiguration integration with springboot application

--- src/main/kotlin/cherhy/jung/gptinterview/GptInterviewApplication.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/GptInterviewApplication.kt
@@ -4,19 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.FilterType
 
-@ComponentScan(
-    excludeFilters = [
-        ComponentScan.Filter(
-            type = FilterType.ASSIGNABLE_TYPE,
-            classes = [FlywayAutoConfiguration::class],
-        )
-    ]
-)
 @ConfigurationPropertiesScan("cherhy.jung.gptinterview.property")
-@SpringBootApplication
+@SpringBootApplication(exclude = [
+    FlywayAutoConfiguration::class
+])
 class GptInterviewApplication
 
 fun main(args: Array<String>) {
