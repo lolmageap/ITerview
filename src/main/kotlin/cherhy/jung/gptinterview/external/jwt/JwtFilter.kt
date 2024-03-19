2cd5fcb4cc9c963c1d9068f0f4ab7b6417aa5ed4
lolmageap <ekxk1234@naver.com>
2024-03-19 22:20:34 +0900

    remove getResolveToken method

--- src/main/kotlin/cherhy/jung/gptinterview/external/jwt/JwtFilter.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/external/jwt/JwtFilter.kt
@@ -2,49 +2,33 @@ package cherhy.jung.gptinterview.external.jwt
 
 import cherhy.jung.gptinterview.extension.authorization
 import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletException
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.security.access.AccessDeniedException
-import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.GenericFilterBean
-import java.io.IOException
+import javax.security.sasl.AuthenticationException
 
 class JwtFilter(
     private val tokenProvider: TokenProvider,
 ) : GenericFilterBean() {
-    @Throws(IOException::class, ServletException::class)
     override fun doFilter(
         servletRequest: ServletRequest,
         servletResponse: ServletResponse?,
         filterChain: FilterChain
     ) {
         val httpServletRequest: HttpServletRequest = servletRequest as HttpServletRequest
-        val jwt: String? = getResolveToken(httpServletRequest)
 
-        val authentication: Authentication? =
-            jwt?.let {
-                if (tokenProvider.validateToken(jwt)) {
-                    tokenProvider.getAuthentication(jwt)
+        val authentication =
+            httpServletRequest.authorization.let {
+                if (tokenProvider.validateToken(it)) {
+                    tokenProvider.getAuthentication(it)
                 } else {
-                    throw AccessDeniedException("토큰이 문제가 생겼습니다.")
+                    throw AuthenticationException()
                 }
             }
 
         SecurityContextHolder.getContext().authentication = authentication
         filterChain.doFilter(servletRequest, servletResponse)
-    }
-
-    private fun getResolveToken(request: HttpServletRequest): String? {
-        val bearerToken: String? = request.authorization
-        bearerToken?.let {
-            if (bearerToken.startsWith("Bearer ")) {
-                return bearerToken.substring(7)
-            }
-            else throw AccessDeniedException("잘못된 토큰")
-        }
-        return null
     }
 }
