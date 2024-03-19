fde04cacc968d126a989d7a795e25290223a619c
lolmageap <ekxk1234@naver.com>
2024-03-19 22:22:30 +0900

    fix to generate refresh token and access token

--- src/main/kotlin/cherhy/jung/gptinterview/extension/HttpServletExtension.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/extension/HttpServletExtension.kt
@@ -1,16 +1,30 @@
 package cherhy.jung.gptinterview.extension
 
+import cherhy.jung.gptinterview.exception.DomainName
+import cherhy.jung.gptinterview.exception.NotFoundException
+import jakarta.servlet.http.Cookie
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import javax.security.sasl.AuthenticationException
 
-val HttpServletRequest.refreshToken: String?
-    get() = this.getHeader("Refresh-Token")
+val HttpServletRequest.refreshToken: String
+    get() = this.cookies.find { it.name == "Refresh-Token" }
+        ?.value?.substringAfter("Bearer ")
+        ?: throw NotFoundException(DomainName.REFRESH_TOKEN)
 
-val HttpServletRequest.authorization: String?
+val HttpServletRequest.authorization: String
     get() = this.getHeader("Authorization")
+        ?.substringAfter("Bearer ")
+        ?: throw AuthenticationException()
 
 fun HttpServletResponse.addAccessTokenInHeader(value: String) =
     this.addHeader("Access-Token", "Bearer $value")
 
-fun HttpServletResponse.addRefreshTokenInHeader(value: String) =
-    this.addHeader("Refresh-Token", "Bearer $value")
+fun HttpServletResponse.addRefreshTokenInCookie(value: String) =
+    this.addCookie(
+        Cookie("Refresh-Token", value).apply {
+            path = "/"
+            isHttpOnly = true
+            maxAge = 60 * 60 * 24 * 30
+        }
+    )
