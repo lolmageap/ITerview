b63e29ca41be720511ef0979ad93aecc25d0d328
lolmageap <ekxk1234@naver.com>
2024-03-21 23:51:13 +0900

    add gpt property

--- src/main/kotlin/cherhy/jung/gptinterview/property/GptProperty.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/property/GptProperty.kt
@@ -5,4 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties("gpt")
 data class GptProperty(
     val apiKey: String,
+    val model: String,
+    val maxTokens: Int,
+    val temperature: Float,
 )
