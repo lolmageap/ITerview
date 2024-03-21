751229f64546699b13ec2d7c5d2465ddb93aa521
lolmageap <ekxk1234@naver.com>
2024-03-21 23:50:53 +0900

    create constant

---
+++ src/main/kotlin/cherhy/jung/gptinterview/external/gpt/GptKey.kt
@@ -0,0 +1,9 @@
+package cherhy.jung.gptinterview.external.gpt
+
+object GptKey {
+    const val MODEL = "model"
+    const val PROMPT = "prompt"
+    const val TEMPERATURE = "temperature"
+    const val MAX_TOKENS = "max_tokens"
+    const val ENDPOINT: String = "https://api.openai.com/v1/completions"
+}
