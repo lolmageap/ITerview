2060d89b0582125cb85546ced01703abf618a2ae
lolmageap <ekxk1234@naver.com>
2024-03-22 15:43:44 +0900

    rename to GPT_ENDPOINT

--- src/main/kotlin/cherhy/jung/gptinterview/external/gpt/GptKey.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/external/gpt/GptKey.kt
@@ -5,5 +5,5 @@ object GptKey {
     const val PROMPT = "prompt"
     const val TEMPERATURE = "temperature"
     const val MAX_TOKENS = "max_tokens"
-    const val ENDPOINT: String = "https://api.openai.com/v1/completions"
+    const val GPT_ENDPOINT: String = "https://api.openai.com/v1/completions"
 }
