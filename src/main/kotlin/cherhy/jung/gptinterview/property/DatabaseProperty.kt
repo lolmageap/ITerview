9dbfee6cb1518f3c663984e356f30649fbd1a3d3
lolmageap <ekxk1234@naver.com>
2024-03-28 22:51:56 +0900

    add DependsOn annotation

--- src/main/kotlin/cherhy/jung/gptinterview/property/DatabaseProperty.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/property/DatabaseProperty.kt
@@ -1,7 +1,9 @@
 package cherhy.jung.gptinterview.property
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.DependsOn
 
+@DependsOn("jasyptStringEncryptor")
 @ConfigurationProperties("spring.datasource")
 data class DatabaseProperty(
     var url: String,
--- src/main/kotlin/cherhy/jung/gptinterview/property/GptProperty.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/property/GptProperty.kt
@@ -1,7 +1,9 @@
 package cherhy.jung.gptinterview.property
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.DependsOn
 
+@DependsOn("jasyptStringEncryptor")
 @ConfigurationProperties("gpt")
 data class GptProperty(
     val apiKey: String,
