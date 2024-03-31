37521c90fbfd972cf6018ec58796d9a9fe415b06
lolmageap <ekxk1234@naver.com>
2024-03-31 22:23:53 +0900

    create oauth2 key

---
+++ src/main/kotlin/cherhy/jung/gptinterview/external/oauth2/OauthKey.kt
@@ -0,0 +1,9 @@
+package cherhy.jung.gptinterview.external.oauth2
+
+object OauthKey {
+    const val GRANT_TYPE = "grant_type"
+    const val CLIENT_ID = "client_id"
+    const val REDIRECT_URI = "redirect_uri"
+    const val CLIENT_SECRET = "client_secret"
+    const val CODE = "code"
+}
