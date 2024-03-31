8d5629b19dd671aa04eb85ea609093985924d15f
lolmageap <ekxk1234@naver.com>
2024-03-31 22:19:38 +0900

    rename to convert user details

--- src/main/kotlin/cherhy/jung/gptinterview/extension/AuthenticationManagerBuilderExtension.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/extension/AuthenticationManagerBuilderExtension.kt
@@ -4,7 +4,7 @@ import cherhy.jung.gptinterview.domain.authority.AuthCustomer
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 
-fun AuthenticationManagerBuilder.getUserDetails(
+fun AuthenticationManagerBuilder.convertUserDetails(
     authenticationToken: UsernamePasswordAuthenticationToken,
 ): AuthCustomer {
     val authenticate = this.`object`.authenticate(authenticationToken)
