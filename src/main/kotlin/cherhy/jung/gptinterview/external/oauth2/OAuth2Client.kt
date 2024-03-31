22b195d3509ad84660c8d2fc37683bf2df33a424
lolmageap <ekxk1234@naver.com>
2024-03-31 22:24:01 +0900

    create oauth2 client

---
+++ src/main/kotlin/cherhy/jung/gptinterview/external/oauth2/OAuth2Client.kt
@@ -0,0 +1,139 @@
+package cherhy.jung.gptinterview.external.oauth2
+
+import cherhy.jung.gptinterview.domain.authority.Oauth2WriteService
+import cherhy.jung.gptinterview.domain.authority.OauthRequest
+import cherhy.jung.gptinterview.domain.customer.Provider
+import cherhy.jung.gptinterview.extension.*
+import cherhy.jung.gptinterview.external.jwt.TokenResponse
+import cherhy.jung.gptinterview.external.oauth2.OauthKey.CLIENT_ID
+import cherhy.jung.gptinterview.external.oauth2.OauthKey.CLIENT_SECRET
+import cherhy.jung.gptinterview.external.oauth2.OauthKey.CODE
+import cherhy.jung.gptinterview.external.oauth2.OauthKey.GRANT_TYPE
+import cherhy.jung.gptinterview.external.oauth2.OauthKey.REDIRECT_URI
+import cherhy.jung.gptinterview.property.GoogleProperty
+import cherhy.jung.gptinterview.property.KakaoClientProperty
+import cherhy.jung.gptinterview.property.KakaoProviderProperty
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.*
+import org.springframework.stereotype.Component
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestTemplate
+
+@Component
+class OAuth2Client(
+    private val kakaoClientProperty: KakaoClientProperty,
+    private val kakaoProviderProperty: KakaoProviderProperty,
+    private val oauth2WriteService: Oauth2WriteService,
+    private val googleProperty: GoogleProperty,
+) {
+    fun signInByKakao(
+        code: String,
+    ): TokenResponse {
+        val restTemplate = RestTemplate()
+        val objectMapper = ObjectMapper()
+
+        val parameters = LinkedMultiValueMap<String, String>().apply {
+            add(GRANT_TYPE, kakaoClientProperty.authorizationGrantType)
+            add(CLIENT_ID, kakaoClientProperty.clientId)
+            add(REDIRECT_URI, kakaoClientProperty.redirectUri)
+            add(CLIENT_SECRET, kakaoClientProperty.clientSecret)
+            add(CODE, code)
+        }
+
+        val headers = HttpHeaders().apply {
+            contentType = MediaType.APPLICATION_FORM_URLENCODED
+        }
+
+        val request: HttpEntity<MultiValueMap<String, String>> = HttpEntity(parameters, headers)
+
+        val response = restTemplate.postForEntity(
+            kakaoProviderProperty.tokenUri,
+            request,
+            String::class.java,
+        )
+
+        val accessToken = objectMapper.getAccessToken(response.body)
+
+        val userInfoHeaders = HttpHeaders().apply {
+            add("Authorization", "Bearer $accessToken")
+        }
+
+        val userInfoRequest = HttpEntity(null, userInfoHeaders)
+
+        val userResponse = restTemplate.exchange(
+            kakaoProviderProperty.userInfoUri,
+            HttpMethod.GET,
+            userInfoRequest,
+            String::class.java,
+        )
+
+        val token = objectMapper.readTree(userResponse.body)
+        val oauthRequest = OauthRequest(
+            providerId = token.id,
+            provider = Provider.KAKAO,
+        )
+
+        return oauth2WriteService.execute(oauthRequest)
+    }
+
+    fun signInByGoogle(
+        code: String,
+    ): TokenResponse {
+        val restTemplate = RestTemplate()
+        val objectMapper = ObjectMapper()
+
+        val headers = HttpHeaders().apply {
+            contentType = MediaType.APPLICATION_FORM_URLENCODED
+            charset(Charsets.UTF_8.name())
+        }
+
+        val parameters = LinkedMultiValueMap<String, String>().apply {
+            add(GRANT_TYPE, googleProperty.authorizationGrantType)
+            add(CLIENT_ID, googleProperty.clientId)
+            add(REDIRECT_URI, googleProperty.redirectUri)
+            add(CLIENT_SECRET, googleProperty.clientSecret)
+            add(CODE, code)
+        }
+
+        val request = HttpEntity(parameters, headers)
+
+        val response = restTemplate.postForEntity(
+            googleProperty.tokenUri,
+            request,
+            String::class.java,
+        )
+
+        val accessToken = objectMapper.getAccessToken(response.body)
+
+        val userInfoHeaders = HttpHeaders().apply {
+            add("Authorization", "Bearer $accessToken")
+        }
+
+        val userInfoRequest = HttpEntity(null, userInfoHeaders)
+
+        val userResponse = restTemplate.exchange(
+            googleProperty.userInfoUri,
+            HttpMethod.GET,
+            userInfoRequest,
+            String::class.java,
+        )
+
+        val token = objectMapper.readTree(userResponse.body)
+
+        val oauthRequest = OauthRequest(
+            providerId = token.id,
+            provider = Provider.GOOGLE,
+        )
+
+        return oauth2WriteService.execute(oauthRequest)
+    }
+
+    fun signInByGithub(code: String): TokenResponse {
+        TODO("Not yet implemented")
+    }
+
+    fun signInByNaver(code: String): TokenResponse {
+        TODO("Not yet implemented")
+    }
+}
