6f47086842fce380d0b96fc719bc72077454a7e5
lolmageap <ekxk1234@naver.com>
2024-04-01 21:48:51 +0900

    add fastapi questions

--- src/main/resources/db/migration/V1.0.23__FASTAPI_QUESTION.sql
+++ src/main/resources/db/migration/V1.0.23__FASTAPI_QUESTION.sql
@@ -1,61 +1,5 @@
 INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI란 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL1');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI의 주요 특징은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL1');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI는 Flask와 Django에 비해 어떻게 다른가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI는 데이터 유효성 검사와 직렬화를 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), '경로 매개변수란 무엇이며, FastAPI에서는 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 쿼리 매개변수는 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL1');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI는 의존성을 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 데이터베이스는 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), 'FastAPI는 의존성(Dependency Injection)을 설명해주세요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL1');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -71,71 +15,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
 VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
 
 INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 요청 파일은 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 오류는 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 자동 API 문서화는 어떻게 설정하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
 VALUES (0, NOW(), NOW(), 'FastAPI에서 Pydantic 모델의 역할은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 백그라운드 작업은 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 SQLAlchemy는 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 보안과 권한은 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 요청 제한은 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 템플릿 엔진은 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -151,30 +31,6 @@ INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
 VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
 
 INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 페이징은 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 자동 모델 직렬화는 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 환경 변수는 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
-
-SET @last_id_in_question = LAST_INSERT_ID();
-
-INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
-VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
-
-INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
 VALUES (0, NOW(), NOW(), 'FastAPI에서 중첩 모델은 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
@@ -183,7 +39,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
 VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
 
 INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 async와 await는 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
+VALUES (0, NOW(), NOW(), 'FastAPI와 Starlette의 차이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -191,7 +47,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
 VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
 
 INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 지역화는 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
+VALUES (0, NOW(), NOW(), 'FastAPI에서 타입 힌트(Type Hints)를 사용하는 이점은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL1');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -199,7 +55,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
 VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
 
 INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 로깅은 어떻게 사용하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+VALUES (0, NOW(), NOW(), 'FastAPI에서 API 레이트 리밋(Rate Limiting)을 구현하는 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -207,7 +63,7 @@ INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
 VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
 
 INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 버전 관리는 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
+VALUES (0, NOW(), NOW(), 'FastAPI의 ASGI(Asynchronous Server Gateway Interface) 지원은 어떤 의미가 있으며, 이것이 성능과 확장성에 어떤 영향을 미치나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
@@ -215,7 +71,47 @@ INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
 VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
 
 INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
-VALUES (0, NOW(), NOW(), 'FastAPI에서 이메일 발송은 어떻게 처리하나요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
+VALUES (0, NOW(), NOW(), 'FastAPI에서 멀티플 라우터(Multiple Routers)를 관리하는 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+
+SET @last_id_in_question = LAST_INSERT_ID();
+
+INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
+VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
+
+INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'FastAPI에서 동적 경로(Operation ID)를 정의하는 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+
+SET @last_id_in_question = LAST_INSERT_ID();
+
+INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
+VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
+
+INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'FastAPI에서 요청 및 응답 훅(Hooks)을 사용하는 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+
+SET @last_id_in_question = LAST_INSERT_ID();
+
+INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
+VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
+
+INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'FastAPI에서 클라이언트 요청 제한(Client Request Limits)을 설정하는 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+
+SET @last_id_in_question = LAST_INSERT_ID();
+
+INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
+VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
+
+INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'FastAPI에서 애플리케이션 상태 및 메트릭스를 모니터링하는 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL2');
+
+SET @last_id_in_question = LAST_INSERT_ID();
+
+INSERT INTO framework (id, question_id, framework_type, created_at, updated_at)
+VALUES (0, @last_id_in_question, 'FAST_API', NOW(), NOW());
+
+INSERT INTO question (id, created_at, updated_at, title, token, question_type, level)
+VALUES (0, NOW(), NOW(), 'FastAPI를 사용하여 RESTful API와 GraphQL API를 동시에 구축할 수 있는 방법은 무엇인가요?', SUBSTRING(UUID(), 1, 8), 'FRAMEWORK', 'LEVEL3');
 
 SET @last_id_in_question = LAST_INSERT_ID();
 
