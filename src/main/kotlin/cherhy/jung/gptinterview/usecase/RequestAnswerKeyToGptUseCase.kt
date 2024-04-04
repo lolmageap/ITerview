dabbc739bd5b45c612559266089caee4ea1127a3
lolmageap <ekxk1234@naver.com>
2024-04-04 23:43:18 +0900

    fix request answer use case

--- src/main/kotlin/cherhy/jung/gptinterview/usecase/RequestAnswerKeyToGptUseCase.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/usecase/RequestAnswerKeyToGptUseCase.kt
@@ -2,10 +2,12 @@ package cherhy.jung.gptinterview.usecase
 
 import cherhy.jung.gptinterview.annotation.UseCase
 import cherhy.jung.gptinterview.domain.customer.CustomerReadService
+import cherhy.jung.gptinterview.domain.question.AnswerWriteService
 import cherhy.jung.gptinterview.external.gpt.GptClient
 import cherhy.jung.gptinterview.external.gpt.GptResponseVo
 import cherhy.jung.gptinterview.domain.question.QuestionHistoryWriteService
 import cherhy.jung.gptinterview.domain.question.QuestionReadService
+import cherhy.jung.gptinterview.domain.question.entity.Answer
 import cherhy.jung.gptinterview.domain.question.entity.QuestionHistory
 import cherhy.jung.gptinterview.util.Generator
 
@@ -14,6 +16,7 @@ class RequestAnswerKeyToGptUseCase(
     private val gptClient: GptClient,
     private val customerReadService: CustomerReadService,
     private val questionReadService: QuestionReadService,
+    private val answerWriteService: AnswerWriteService,
     private val questionHistoryWriteService: QuestionHistoryWriteService,
 ) {
     fun execute(customerId: Long, token: String): GptResponseVo {
@@ -24,7 +27,10 @@ class RequestAnswerKeyToGptUseCase(
 
         val feedback = gptClient.requestAndReceiveFeedback(questionToGpt)
 
-        val questionHistory = QuestionHistory.of(question.id, customer.id, feedback)
+        val answer = Answer.of(customer.id, question.id, feedback)
+        val saveAnswer = answerWriteService.addAnswer(answer)
+
+        val questionHistory = QuestionHistory.of(question.id, customer.id, saveAnswer.id)
         val history = questionHistoryWriteService.addHistory(questionHistory)
 
         return GptResponseVo(history.token, feedback)
--- src/main/kotlin/cherhy/jung/gptinterview/usecase/RequestAnswerToGptUseCase.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/usecase/RequestAnswerToGptUseCase.kt
@@ -7,6 +7,8 @@ import cherhy.jung.gptinterview.external.gpt.GptResponseVo
 import cherhy.jung.gptinterview.domain.question.QuestionHistoryWriteService
 import cherhy.jung.gptinterview.domain.question.QuestionReadService
 import cherhy.jung.gptinterview.controller.dto.GptRequest
+import cherhy.jung.gptinterview.domain.question.AnswerWriteService
+import cherhy.jung.gptinterview.domain.question.entity.Answer
 import cherhy.jung.gptinterview.domain.question.entity.QuestionHistory
 import cherhy.jung.gptinterview.util.Generator
 
@@ -15,6 +17,7 @@ class RequestAnswerToGptUseCase(
     private val gptClient: GptClient,
     private val customerReadService: CustomerReadService,
     private val questionReadService: QuestionReadService,
+    private val answerWriteService: AnswerWriteService,
     private val questionHistoryWriteService: QuestionHistoryWriteService,
 ) {
     fun execute(customerId: Long, request: GptRequest): GptResponseVo {
@@ -25,7 +28,10 @@ class RequestAnswerToGptUseCase(
 
         val feedback = gptClient.requestAndReceiveFeedback(questionToGpt)
 
-        val questionHistory = QuestionHistory.of(question.id, customer.id, feedback)
+        val answer = Answer.of(customer.id, question.id, feedback, request.answer)
+        val saveAnswer = answerWriteService.addAnswer(answer)
+
+        val questionHistory = QuestionHistory.of(question.id, customer.id, saveAnswer.id)
         val history = questionHistoryWriteService.addHistory(questionHistory)
 
         return GptResponseVo(history.token, feedback)
