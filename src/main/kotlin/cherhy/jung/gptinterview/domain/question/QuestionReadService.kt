de7eaff785f7b350125d2d5ab7f66892160729b3
lolmageap <ekxk1234@naver.com>
2024-04-04 23:46:25 +0900

    add get question by id

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionReadService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionReadService.kt
@@ -5,6 +5,7 @@ import cherhy.jung.gptinterview.domain.question.vo.QuestionRequestVo
 import cherhy.jung.gptinterview.domain.question.vo.QuestionResponseVo
 import cherhy.jung.gptinterview.exception.MessageType.QUESTION
 import cherhy.jung.gptinterview.exception.NotFoundException
+import org.springframework.data.repository.findByIdOrNull
 
 @ReadService
 class QuestionReadService(
@@ -28,4 +29,9 @@ class QuestionReadService(
     fun getQuestionHistories(alreadyQuestions: List<String>): List<QuestionResponseVo> =
         questionRepository.findByTokensIn(alreadyQuestions)
             .map(QuestionResponseVo::of)
+
+    fun getQuestionById(id: Long) =
+        questionRepository.findByIdOrNull(id)
+            ?.let(QuestionResponseVo::of)
+            ?: throw NotFoundException(QUESTION)
 }
