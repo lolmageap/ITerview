2c274af06eeb8af8ad965008040d2ef0071ba095
lolmageap <ekxk1234@naver.com>
2024-04-27 23:54:45 +0900

    add get question history logic

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryReadService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionHistoryReadService.kt
@@ -1,7 +1,11 @@
 package cherhy.jung.gptinterview.domain.question
 
 import cherhy.jung.gptinterview.annotation.ReadService
+import cherhy.jung.gptinterview.domain.question.vo.QuestionHistoryResponseVo
+import cherhy.jung.gptinterview.exception.MessageType
+import cherhy.jung.gptinterview.exception.NotFoundException
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
 
 @ReadService
 class QuestionHistoryReadService(
@@ -12,5 +16,10 @@ class QuestionHistoryReadService(
 
     fun getQuestionHistory(customerId: Long, token: String) =
         questionHistoryRepository.findByCustomerIdAndToken(customerId, token)
+
+    fun getQuestionHistoryById(questionHistoryId: Long) =
+        questionHistoryRepository.findByIdOrNull(questionHistoryId)
+            ?.let(QuestionHistoryResponseVo::of)
+            ?: throw NotFoundException(MessageType.QUESTION_HISTORY)
 }
 
