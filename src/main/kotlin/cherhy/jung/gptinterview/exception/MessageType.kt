9a01be37e9d149ba6271ba69353e0a4f04fd4d4f
lolmageap <ekxk1234@naver.com>
2024-04-27 23:54:27 +0900

    add error message constant

--- src/main/kotlin/cherhy/jung/gptinterview/exception/MessageType.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/exception/MessageType.kt
@@ -3,6 +3,7 @@ package cherhy.jung.gptinterview.exception
 enum class MessageType(val value: String) {
     CUSTOMER("회원"),
     QUESTION("질문"),
+    QUESTION_HISTORY("질문 이력"),
     EMAIL("이메일"),
     CERTIFICATE_NUMBER("인증 번호"),
     REFRESH_TOKEN("리프레시 토큰"),
