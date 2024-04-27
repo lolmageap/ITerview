50b96803a62abbb8109267571ae5559cd04332fe
lolmageap <ekxk1234@naver.com>
2024-04-27 23:58:07 +0900

    fix to generate re answer and business logic

--- src/main/kotlin/cherhy/jung/gptinterview/usecase/RequestReAnswerToGptUseCase.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/usecase/RequestReAnswerToGptUseCase.kt
@@ -3,10 +3,7 @@ package cherhy.jung.gptinterview.usecase
 import cherhy.jung.gptinterview.annotation.UseCase
 import cherhy.jung.gptinterview.controller.dto.GptReAnswerRequest
 import cherhy.jung.gptinterview.domain.customer.CustomerReadService
-import cherhy.jung.gptinterview.domain.question.AnswerWriteService
-import cherhy.jung.gptinterview.domain.question.FeedbackReadService
-import cherhy.jung.gptinterview.domain.question.FeedbackWriteService
-import cherhy.jung.gptinterview.domain.question.QuestionReadService
+import cherhy.jung.gptinterview.domain.question.*
 import cherhy.jung.gptinterview.domain.question.entity.Answer
 import cherhy.jung.gptinterview.domain.question.entity.Feedback
 import cherhy.jung.gptinterview.external.gpt.GptClient
@@ -16,7 +13,7 @@ import cherhy.jung.gptinterview.util.Generator
 @UseCase
 class RequestReAnswerToGptUseCase(
     private val gptClient: GptClient,
-    private val questionReadService: QuestionReadService,
+    private val questionHistoryReadService: QuestionHistoryReadService,
     private val customerReadService: CustomerReadService,
     private val feedbackReadService: FeedbackReadService,
     private val answerWriteService: AnswerWriteService,
@@ -24,16 +21,17 @@ class RequestReAnswerToGptUseCase(
 ) {
     fun execute(customerId: Long, request: GptReAnswerRequest): GptResponseVo {
         val customer = customerReadService.getCustomerById(customerId)
-        val feedback = feedbackReadService.getFeedbackByToken(request.questionToken)
-        val question = questionReadService.getQuestionById(feedback.questionId)
-        val questionToGpt = Generator.reAnswerToGpt(question.title, feedback.text, request.answer)
+        val feedback = feedbackReadService.getFeedbackByToken(request.feedbackToken)
+        val history = questionHistoryReadService.getQuestionHistoryById(feedback.questionHistoryId)
 
-        val answer = Answer.of(customer.id, question.id, request.answer)
+        val questionToGpt = Generator.reAnswerToGpt(history.question, feedback.text, request.answer)
+
+        val answer = Answer.of(customer.id, history.id, request.answer)
         val savedAnswer = answerWriteService.addAnswer(answer)
 
         val newFeedback = gptClient.requestAndReceiveFeedback(questionToGpt)
 
-        val feedbackEntity = Feedback.of(customer.id, question.id, savedAnswer.id, newFeedback)
+        val feedbackEntity = Feedback.of(customer.id, history.id, savedAnswer.id, newFeedback)
         feedbackWriteService.addFeedback(feedbackEntity)
 
         return GptResponseVo(feedback.token, newFeedback)
