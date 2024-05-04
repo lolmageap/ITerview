69ad1573d0a04028e270a167104874cdded13043
lolmageap <ekxk1234@naver.com>
2024-05-04 23:40:26 +0900

    create jpa config with thread local

--- src/main/kotlin/cherhy/jung/gptinterview/config/JpaConfig.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/config/JpaConfig.kt
@@ -1,8 +1,30 @@
 package cherhy.jung.gptinterview.config
 
+import jakarta.persistence.PostUpdate
+import jakarta.persistence.PreRemove
+import jakarta.persistence.PreUpdate
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @Configuration
 @EnableJpaAuditing
-class JpaConfig
+class JpaConfig {
+
+    @PreUpdate
+    fun preUpdateEvent(target: Any) {
+        val userId = ThreadLocalConfig().userId
+        println("userId = $userId")
+        println("target = $target")
+    }
+
+    @PostUpdate
+    fun postUpdateEvent(target: Any) {
+
+    }
+
+    @PreRemove
+    fun preRemoveEvent(target: Any) {
+
+    }
+
+}
