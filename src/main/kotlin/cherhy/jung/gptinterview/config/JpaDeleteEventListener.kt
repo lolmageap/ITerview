a9189839dd019d1fbc9793add7c4fb929e3cd69e
lolmageap <ekxk1234@naver.com>
2024-05-10 23:37:58 +0900

    create to customer delete history

--- src/main/kotlin/cherhy/jung/gptinterview/config/JpaDeleteEventListener.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/config/JpaDeleteEventListener.kt
@@ -1,12 +1,49 @@
 package cherhy.jung.gptinterview.config
 
+import cherhy.jung.gptinterview.domain.authority.Principal
+import cherhy.jung.gptinterview.domain.customer.CustomerHistory
+import cherhy.jung.gptinterview.domain.customer.HistoryType
+import cherhy.jung.gptinterview.extension.classDescription
+import cherhy.jung.gptinterview.extension.className
+import cherhy.jung.gptinterview.extension.extractFields
+import cherhy.jung.gptinterview.extension.targetCustomerId
+import cherhy.jung.gptinterview.util.Entity
 import jakarta.persistence.PreRemove
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
 
 @Component
 class JpaDeleteEventListener() {
-    @PreRemove
-    fun preRemoveEvent(target: Any) {
+    private lateinit var threadLocal: ThreadLocal<Principal>
+    private lateinit var applicationEventPublisher: ApplicationEventPublisher
 
+    constructor(
+        threadLocal: ThreadLocal<Principal>,
+        applicationEventPublisher: ApplicationEventPublisher,
+    ) : this() {
+        this.threadLocal = threadLocal
+        this.applicationEventPublisher = applicationEventPublisher
+    }
+
+    @PreRemove
+    fun preRemoveEvent(entity: Entity) {
+        val principal = threadLocal.get() ?: return
+        val customerId = principal.id
+
+        val deletedFields = entity.extractFields()
+        deletedFields.forEach { (fieldName, deletedValue) ->
+            CustomerHistory.of(
+                customerId = customerId,
+                targetCustomerId = entity.targetCustomerId,
+                type = HistoryType.DELETE,
+                entityName = entity.className,
+                entityDescription = entity.classDescription,
+                fieldName = fieldName,
+                fieldDescription = deletedValue.description,
+                beforeValue = null,
+                afterValue = deletedValue.value.toString(),
+            )
+        }
+        applicationEventPublisher.publishEvent(deletedFields)
     }
 }
