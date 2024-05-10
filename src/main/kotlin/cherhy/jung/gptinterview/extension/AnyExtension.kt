e23b2d31499e7bb79e0e583447c97397002f68a8
lolmageap <ekxk1234@naver.com>
2024-05-10 23:37:04 +0900

    separate entity type and function

---
+++ src/main/kotlin/cherhy/jung/gptinterview/extension/AnyExtension.kt
@@ -0,0 +1,33 @@
+package cherhy.jung.gptinterview.extension
+
+import cherhy.jung.gptinterview.annotation.Description
+import cherhy.jung.gptinterview.annotation.LoggingDisable
+import cherhy.jung.gptinterview.annotation.TargetCustomerId
+import cherhy.jung.gptinterview.config.ChangeValueCollector
+import cherhy.jung.gptinterview.util.Entity
+
+val Entity.className: String
+    get() = this.javaClass.simpleName
+
+fun Entity.extractFields() =
+    this.javaClass.declaredFields.mapNotNull { field ->
+        field.isAccessible = true
+        field.getAnnotation(LoggingDisable::class.java)
+            ?.let { return@mapNotNull null }
+
+        val value = field.get(this)
+        val description = field.getAnnotation(Description::class.java)?.value
+
+        field.name to ChangeValueCollector(value, description)
+    }.toMap()
+
+val Entity.targetCustomerId: Long
+    get() = this.javaClass.declaredFields
+        .first { it.getAnnotation(TargetCustomerId::class.java) != null }
+        .let { field ->
+            field.isAccessible = true
+            field.get(this) as Long
+        }
+
+val Entity.classDescription: String?
+    get() = this.javaClass.getAnnotation(Description::class.java)?.value
---
+++ src/main/kotlin/cherhy/jung/gptinterview/util/TypeAlias.kt
@@ -0,0 +1,5 @@
+package cherhy.jung.gptinterview.util
+
+typealias Entity = Any
+typealias UserKey = String
+typealias FieldName = String
