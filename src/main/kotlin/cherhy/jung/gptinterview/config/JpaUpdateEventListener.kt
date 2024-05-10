e0d7850ff4b53fa4a161404f858cf379295dee69
lolmageap <ekxk1234@naver.com>
2024-05-10 23:38:07 +0900

    create to customer update history

--- src/main/kotlin/cherhy/jung/gptinterview/config/JpaUpdateEventListener.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/config/JpaUpdateEventListener.kt
@@ -1,46 +1,53 @@
 package cherhy.jung.gptinterview.config
 
-import cherhy.jung.gptinterview.annotation.Description
-import cherhy.jung.gptinterview.annotation.LoggingDisable
 import cherhy.jung.gptinterview.domain.authority.Principal
+import cherhy.jung.gptinterview.domain.customer.CustomerHistory
+import cherhy.jung.gptinterview.domain.customer.HistoryType
+import cherhy.jung.gptinterview.extension.classDescription
+import cherhy.jung.gptinterview.extension.extractFields
+import cherhy.jung.gptinterview.extension.className
+import cherhy.jung.gptinterview.extension.targetCustomerId
+import cherhy.jung.gptinterview.external.cache.CacheReadService
 import cherhy.jung.gptinterview.external.cache.CacheWriteService
+import cherhy.jung.gptinterview.util.Entity
+import cherhy.jung.gptinterview.util.FieldName
+import cherhy.jung.gptinterview.util.UserKey
 import jakarta.persistence.PostLoad
 import jakarta.persistence.PostUpdate
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
 
-typealias Entity = Any
-typealias UserKey = String
-typealias FieldName = String
-
 @Component
 class JpaUpdateEventListener() {
     private lateinit var threadLocal: ThreadLocal<Principal>
     private lateinit var applicationEventPublisher: ApplicationEventPublisher
     private lateinit var cacheWriteService: CacheWriteService
-    val entityMap = ConcurrentHashMap<UserKey, Map<FieldName, Log>>()
+    private lateinit var cacheReadService: CacheReadService
+    val entityMap = ConcurrentHashMap<UserKey, Map<FieldName, ChangeValueCollector>>()
 
     constructor(
         threadLocal: ThreadLocal<Principal>,
         applicationEventPublisher: ApplicationEventPublisher,
         cacheWriteService: CacheWriteService,
+        cacheReadService: CacheReadService,
     ) : this() {
         this.threadLocal = threadLocal
         this.applicationEventPublisher = applicationEventPublisher
         this.cacheWriteService = cacheWriteService
+        this.cacheReadService = cacheReadService
     }
 
     @PostLoad
     fun preUpdateEvent(entity: Entity) {
         val principal = threadLocal.get() ?: return
         val userId = principal.id
-        val key = "$userId-${entity.name}"
+        val key = "$userId-${entity.className}"
 
         try {
             val stateBeforeUpdate = entity.extractFields()
             entityMap[key] = stateBeforeUpdate
-            cacheWriteService.setEntity(key, stateBeforeUpdate)
+            cacheWriteService.setBeforeValue(key, stateBeforeUpdate)
         } finally {
             entityMap.remove(key)
         }
@@ -49,41 +56,31 @@ class JpaUpdateEventListener() {
     @PostUpdate
     fun postUpdateEvent(entity: Entity) {
         val principal = threadLocal.get() ?: return
-        val userId = principal.id
-        val key = "$userId-${entity.name}"
+        val customerId = principal.id
+        val key = "$customerId-${entity.className}"
 
-        val stateBeforeUpdate = entityMap[key] ?: return
+        val stateBeforeUpdate = cacheReadService.getBeforeValue(key)
         val stateAfterUpdate = entity.extractFields()
 
-        val updatedFields = stateAfterUpdate.filter { (fieldName, log) ->
-            stateBeforeUpdate[fieldName]?.value != log.value
-            // TODO: 비교 로직이 들어감
-        }
+        stateAfterUpdate.forEach { (fieldName, after) ->
+            val beforeValue = stateBeforeUpdate[fieldName]?.value
+            val afterValue = after.value
 
-        if (updatedFields.isNotEmpty()) {
-            TODO("applicationEventPublisher.publishEvent()")
+            if (beforeValue != afterValue) {
+                val updatedFields = CustomerHistory.of(
+                    customerId = customerId,
+                    targetCustomerId = entity.targetCustomerId,
+                    type = HistoryType.UPDATE,
+                    entityName = entity.className,
+                    entityDescription = entity.classDescription,
+                    fieldName = fieldName,
+                    fieldDescription = after.description,
+                    beforeValue = beforeValue.toString(),
+                    afterValue = afterValue.toString(),
+                )
+
+                applicationEventPublisher.publishEvent(updatedFields)
+            }
         }
     }
-
-    private val Entity.name: String
-        get() = this::class.simpleName ?: ""
-
-    private fun Entity.extractFields() =
-        this.javaClass.declaredFields.map { field ->
-            field.isAccessible = true
-            field.getAnnotation(LoggingDisable::class.java)
-                ?.let { return@map null }
-
-            val value = field.get(this)
-            val description = field.getAnnotation(Description::class.java)?.value
-
-            field.name to Log(value, description)
-        }
-            .filterNotNull()
-            .toMap()
-
-    data class Log(
-        val value: Any,
-        val description: String?,
-    )
 }
