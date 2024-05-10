9e05fbda24ee500c5abe3be79808da3bf638e91e
lolmageap <ekxk1234@naver.com>
2024-05-10 23:34:58 +0900

    add logging disable annotation

--- src/main/kotlin/cherhy/jung/gptinterview/annotation/LoggingDisable.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/annotation/LoggingDisable.kt
@@ -2,4 +2,4 @@ package cherhy.jung.gptinterview.annotation
 
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FIELD)
-annotation class LoggingDisable()
+annotation class LoggingDisable
