38b2d735574e542fb8fa37853912f8be4c703079
lolmageap <ekxk1234@naver.com>
2024-05-11 23:51:00 +0900

    remove declare to argument

--- src/main/kotlin/cherhy/jung/gptinterview/exception/AlreadyExistsException.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/exception/AlreadyExistsException.kt
@@ -1,7 +1,7 @@
 package cherhy.jung.gptinterview.exception
 
 class AlreadyExistsException(
-    private val property: MessageType,
+    property: MessageType,
 ) : GlobalRuntimeException(
     message = "${property.value}이(가) 이미 존재 합니다."
 )
--- src/main/kotlin/cherhy/jung/gptinterview/exception/NotFoundException.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/exception/NotFoundException.kt
@@ -1,7 +1,7 @@
 package cherhy.jung.gptinterview.exception
 
 class NotFoundException(
-    private val state: MessageType,
+    state: MessageType,
 ) : GlobalRuntimeException(
     message = "${state.value}이(가) 존재하지 않습니다."
 )
