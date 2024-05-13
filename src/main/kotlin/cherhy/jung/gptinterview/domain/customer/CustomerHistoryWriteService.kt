41829e5e63cde86b8ad1f29a44ce436b9c03e090
lolmageap <ekxk1234@naver.com>
2024-05-13 22:04:40 +0900

    refactor to save customer history

--- src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerHistoryWriteService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerHistoryWriteService.kt
@@ -1,16 +1,17 @@
 package cherhy.jung.gptinterview.domain.customer
 
+import cherhy.jung.gptinterview.annotation.WriteService
 import cherhy.jung.gptinterview.domain.customer.entity.CustomerHistory
 import org.springframework.context.event.EventListener
-import org.springframework.stereotype.Service
 
-@Service
+@WriteService
 class CustomerHistoryWriteService(
     private val customerHistoryRepository: CustomerHistoryRepository,
 ) {
-    @EventListener(CustomerHistory::class)
+    @EventListener(value = [CustomerHistory::class])
     fun saveCustomerHistory(
         customerHistory: CustomerHistory,
-    ) =
+    ) {
         customerHistoryRepository.save(customerHistory)
+    }
 }
