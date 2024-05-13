6165e20b0500d909f3c6f5b0a86c72605320e361
lolmageap <ekxk1234@naver.com>
2024-05-13 22:05:47 +0900

    register to principal interceptor

---
+++ src/main/kotlin/cherhy/jung/gptinterview/config/PrincipalInterceptorConfig.kt
@@ -0,0 +1,14 @@
+package cherhy.jung.gptinterview.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class PrincipalInterceptorConfig(
+    private val principalInterceptor: PrincipalInterceptor,
+) : WebMvcConfigurer {
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(principalInterceptor)
+    }
+}
