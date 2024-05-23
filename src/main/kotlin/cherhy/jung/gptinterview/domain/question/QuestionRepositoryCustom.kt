fc026cd6bbdbd110e0eea1ed9777995db2e8a268
lolmageap <ekxk1234@naver.com>
2024-05-23 21:33:09 +0900

    fix return type to not null

--- src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionRepositoryCustom.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/question/QuestionRepositoryCustom.kt
@@ -6,68 +6,72 @@ import cherhy.jung.gptinterview.domain.question.entity.QQuestion.question
 import cherhy.jung.gptinterview.domain.question.entity.Question
 import cherhy.jung.gptinterview.domain.question.vo.QuestionRequestVo
 import com.querydsl.core.BooleanBuilder
-import com.querydsl.core.types.dsl.BooleanExpression
+import com.querydsl.core.types.dsl.Expressions
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport
 
 // TODO : 퇴근 하고 수정 하기
 interface QuestionRepositoryCustom {
-    fun findByQuestionRequestS(
-        questionRequestS: QuestionRequestVo,
+    fun findAll(
+        request: QuestionRequestVo,
         questionTokens: List<String>,
     ): List<Question>
 
-    fun findByTokensIn(questionTokens: List<String>): List<Question>
+    fun findByTokensIn(
+        questionTokens: List<String>,
+    ): List<Question>
 }
 
-@Suppress("PARAMETER_NAME_CHANGED_ON_OVERRIDE")
-class QuestionRepositoryCustomImpl : QuestionRepositoryCustom, QuerydslRepositorySupport(Question::class.java) {
-    override fun findByQuestionRequestS(
+class QuestionRepositoryCustomImpl : QuestionRepositoryCustom,
+    QuerydslRepositorySupport(Question::class.java) {
+    override fun findAll(
         request: QuestionRequestVo,
-        alreadyQuestion: List<String>,
-    ): List<Question> {
-        val query = from(question)
-
-        return query
-            .where(
-                BooleanBuilder().let { condition ->
-                    checkQuestionType(request)?.let { condition.or(it) }
-                    checkPrograming(request)?.let { condition.or(it) }
-                    checkFramework(request)?.let { condition.or(it) }
-                    checkLevel(request)?.let { condition.and(it) }
-                    condition.and(question.token.notIn(alreadyQuestion))
-                }
-            )
-            .fetch()
-            .also {
-                it.shuffle()
+        questionTokens: List<String>,
+    ): List<Question> = from(question)
+        .where(
+            BooleanBuilder().let { condition ->
+                checkQuestionType(request)?.let { condition.or(it) }
+                checkPrograming(request)?.let { condition.or(it) }
+                checkFramework(request)?.let { condition.or(it) }
+                checkLevel(request)?.let { condition.and(it) }
+                condition.and(question.token.notIn(questionTokens))
             }
-    }
+        )
+        .fetch()
+        .also {
+            it.shuffle()
+        }
 
-    override fun findByTokensIn(alreadyQuestion: List<String>): List<Question> =
+    override fun findByTokensIn(
+        questionTokens: List<String>,
+    ) =
         from(question)
             .where(
-                question.token.`in`(alreadyQuestion),
+                question.token.`in`(questionTokens),
             )
             .fetch()
             .sortedBy {
-                alreadyQuestion.indexOf(it.token)
+                questionTokens.indexOf(it.token)
             }
             .reversed()
 
-    private fun checkLevel(request: QuestionRequestVo): BooleanExpression? {
-        return if (request.levels.isNotEmpty()) {
+    private fun checkLevel(
+        request: QuestionRequestVo,
+    ) =
+        if (request.levels.isNotEmpty()) {
             question.level.`in`(request.levels)
-        } else null
-    }
+        } else Expressions.TRUE
 
-    private fun checkQuestionType(request: QuestionRequestVo): BooleanExpression? {
-        return if (request.questionTypes.isNotEmpty()) {
+    private fun checkQuestionType(
+        request: QuestionRequestVo,
+    ) =
+        if (request.questionTypes.isNotEmpty()) {
             question.questionType.`in`(request.questionTypes)
-        } else null
-    }
+        } else Expressions.TRUE
 
-    private fun checkPrograming(request: QuestionRequestVo): BooleanExpression? {
-        return if (request.programingTypes.isNotEmpty()) {
+    private fun checkPrograming(
+        request: QuestionRequestVo,
+    ) =
+        if (request.programingTypes.isNotEmpty()) {
             from(programing)
                 .join(question)
                 .on(programing.questionId.eq(question.id))
@@ -77,11 +81,12 @@ class QuestionRepositoryCustomImpl : QuestionRepositoryCustom, QuerydslRepositor
                 .let {
                     question.id.`in`(it)
                 }
-        } else null
-    }
+        } else Expressions.TRUE
 
-    private fun checkFramework(request: QuestionRequestVo): BooleanExpression? {
-        return if (request.frameworkTypes.isNotEmpty()) {
+    private fun checkFramework(
+        request: QuestionRequestVo,
+    ) =
+        if (request.frameworkTypes.isNotEmpty()) {
             from(framework)
                 .join(question)
                 .on(framework.questionId.eq(question.id))
@@ -91,6 +96,5 @@ class QuestionRepositoryCustomImpl : QuestionRepositoryCustom, QuerydslRepositor
                 .let {
                     question.id.`in`(it)
                 }
-        } else null
-    }
+        } else Expressions.TRUE
 }
