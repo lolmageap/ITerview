278d8908c67770897f2b8e1d0804bce74050ab55
lolmageap <ekxk1234@naver.com>
2024-06-26 22:44:18 +0900

    fix TransactionalEventListener to EventListener

--- src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerHistoryWriteService.kt
+++ src/main/kotlin/cherhy/jung/gptinterview/domain/customer/CustomerHistoryWriteService.kt
@@ -2,13 +2,13 @@ package cherhy.jung.gptinterview.domain.customer
 
 import cherhy.jung.gptinterview.annotation.WriteService
 import cherhy.jung.gptinterview.domain.customer.entity.CustomerHistory
-import org.springframework.context.event.EventListener
+import org.springframework.transaction.event.TransactionalEventListener
 
 @WriteService
 class CustomerHistoryWriteService(
     private val customerHistoryRepository: CustomerHistoryRepository,
 ) {
-    @EventListener(value = [CustomerHistory::class])
+    @TransactionalEventListener(value = [CustomerHistory::class])
     fun saveCustomerHistory(
         customerHistory: CustomerHistory,
     ) {
